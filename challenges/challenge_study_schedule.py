125b077ac5b7d284956439acb0ceba0e204805fa
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-01-19 18:29:03 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    try:
+        num_of_students = 0
+        for entry_time, exite_time in permanence_period:
+            if entry_time <= target_time <= exite_time:
+                num_of_students += 1
+        return num_of_students
+
+    except TypeError:
+        return None
+
+
+# permanence_period = [(2, "2"), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# print(study_schedule(permanence_period, 5))
