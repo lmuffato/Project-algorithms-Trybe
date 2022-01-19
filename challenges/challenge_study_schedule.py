20616bde6b2755f45fad1d5b13ea470ba39df0db
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-19 14:51:24 -0300

    Feat: Implementa a funçao 'study_schedule'

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    _count = 0
+
+    for period in permanence_period:
+        if period[0] is None or period[1] is None:
+            return None
+        if period[1] >= target_time and period[0] <= target_time:
+            _count += 1
+
+    return _count
