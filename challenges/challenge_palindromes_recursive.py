33e3b607147081f3db5572049132c5e38f20b7db
MarcioFujita <hanker78@gmail.com>
2022-01-19 17:42:05 -0300

    feat: Req 2 try 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,3 +2,13 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if not word:
         return False
+
+    if word[low_index] != word[high_index]:
+        return False
+    elif word[low_index] == word[high_index] and low_index == high_index:
+        return True
+    else:
+        if word[low_index] == word[high_index] and (low_index + 1) == (high_index):
+            return True
+        
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
