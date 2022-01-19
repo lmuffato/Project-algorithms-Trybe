691dc2d66ec45ea6bc75ae2f89942e2e2df5dab1
iago <iago.pferreiravr@gmail.com>
2022-01-19 16:15:36 -0300

    feat: Req4
    Implementando a função duplicate no arquivo challenge_find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,18 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 1:
+        return False
+    nums.sort()
+    mid = 0
+    start = 0
+    end = len(nums)
+    while start <= end:
+        mid = (start + end) // 2
+        try:
+            if not isinstance(nums[mid], int) or nums[mid] < 0:
+                return False
+            if nums[mid] == nums[mid - 1] or nums[mid] == nums[mid + 1]:
+                return nums[mid]
+            else:
+                start += 1
+        except IndexError:
+            return False
