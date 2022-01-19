de93579d747aac98d1f354870d9c3fbf8c071930
Gabriel <g.pereira.a.m@gmail.com>
2022-01-19 19:33:12 -0300

    feat(Req 2) - :sparkle: Requisito 2 feito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    saida = False
+    if word == '':
+        return saida
+    if low_index > high_index:
+        saida = True
+        return saida
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return saida
+
