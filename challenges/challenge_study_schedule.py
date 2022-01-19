1c4236540f22a420a46c9fb5b0068a278797c7f5
Gabriel <g.pereira.a.m@gmail.com>
2022-01-19 17:11:24 -0300

    feat(Req 1) - :sparkle: Requisito 1 feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    ocorrence_target = 0
+
+    if int != type(target_time):
+       return None
+
+    for key, element in permanence_period:
+        if key is None or element is None:
+           return None
+        elif key <= target_time <= element:
+           ocorrence_target += 1
+    return ocorrence_target
