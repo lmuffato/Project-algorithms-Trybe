8ee2d469115d98e20612759ae7cc641356429ccd
Flavio Franco <flavio.win@gmail.com>
2022-01-19 16:34:38 -0300

    requisito 3

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,17 @@
-def find_duplicate(nums):
-    """ Faça o código aqui. """
+def find_duplicate(nums=None):
+    if not nums or len(nums) == 1 or type(nums) == "string":
+        return False
+
+    nums.sort()
+
+    for i in range(len(nums)):
+        if nums[i] < 0:
+            break
+        if nums[i] == nums[i+1]:
+            return nums[i]
+
+    return False
+
+
+print(find_duplicate())
+
