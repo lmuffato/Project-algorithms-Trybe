1622efdfdeb78d7004000b2bb92b65d9b78119f0
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-19 18:05:06 -0300

    requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    contador = 0
+    if target_time is None:
+        return None
+    for period_1, period_2 in permanence_period:
+        if type(period_1) != int or type(period_2) != int:
+            return None
+        elif target_time >= period_1 and target_time <= period_2:
+            contador += 1
+    return contador
