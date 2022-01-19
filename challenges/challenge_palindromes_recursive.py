84199c2772e7b59c029a249fa193bd032c01facf
Ana Ventura <anaventura1811@gmail.com>
2022-01-19 20:35:25 -0300

    fix: remove variáveis desnecessárias

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -3,23 +3,12 @@ def is_palindrome_recursive(word, low_index, high_index):
     word_reversed = ''
     if word:
         word_test = list(word)
-        new_high_index = high_index
-        new_low_index = low_index
         for char in word_test:
             if char == word_test[high_index] and high_index != -1:
                 test.append(char)
                 low_index += 1
                 high_index -= 1
-                new_high_index -= 1
-                new_low_index += 1
-                # print(low_index)
-                # print(high_index)
-                # print(new_low_index)
-                # print(new_high_index)
-                is_palindrome_recursive(word, new_low_index, new_high_index)
-            # low_index += 1
-            # high_index -= 1
-        # print(word_reversed)
+                is_palindrome_recursive(word, low_index, high_index)
         word_reversed = "".join(test)
         if word == word_reversed:
             return True
