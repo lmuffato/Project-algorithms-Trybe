5375a0e0dde8765ee36dc7f2119dc31444743df7
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-19 19:29:00 -0300

    requisito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    result = False
+    if word == "":
+        return result
+    if low_index > high_index:
+        result = True
+        return result
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return result
