7d3628ce60300e5e9a59cfbf524c1af5b0092863
Gui-lira <gui.lira.martins@gmail.com>
2022-01-19 18:28:56 -0300

    3 usando função mais performatica

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+# me baseei na seguinte discussão do stack overflow:
+# https://stackoverflow.com/questions/13101468/python-how-to-sort-the-alphabet-in-a-list-without-sorted-functions
+def quicksort(lst):
+    return (quicksort([letter for letter in lst[1:] if letter < lst[0]])
+            + [lst[0]] +
+            quicksort([letter for letter in lst[1:] if letter >= lst[0]]))
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_list = quicksort(list(first_string))
+    second_list = quicksort(list(second_string))
+    return first_list == second_list
