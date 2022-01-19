d0634ea26014f4a9589eb52f16d4862d3e9cad84
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-19 09:06:40 -0300

    feat(challenge_study_schedule.py): adiciona requisito 1 de algortimo de busca com força bruta

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    if(not target_time):
+        return None
+    for period in permanence_period:
+        low, high = period
+        if(not low or not high):
+            return None
+        if(low <= target_time <= high):
+            counter += 1
+
+    return counter
