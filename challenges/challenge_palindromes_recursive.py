339c4eeb26f27f25760def877ad854a3397d8d35
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-01-19 10:41:31 -0300

    requisito 2 finalizado

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,5 +7,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     if word[low_index] != word[high_index]:
         return False
     if low_index <= size:
-        is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
