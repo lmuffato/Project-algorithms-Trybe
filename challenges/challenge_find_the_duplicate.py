51d30876c5ffe368f6490e34aaefbd8c6801579d
Flavio Franco <flavio.win@gmail.com>
2022-01-19 17:11:52 -0300

    requisito bonus

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,21 +1,29 @@
-def find_duplicate(nums=None):
+def initial_check(nums):
     if not nums or len(nums) == 1 or type(nums) == "string":
         return False
+    return True
 
+def sort_nums(nums):
     try:
         nums.sort()
     except TypeError:
         return False
+    
+    return nums
 
-    for i in range(len(nums)-1):
-        if isinstance(nums[i], str):
+
+def find_duplicate(nums=None):
+    if not initial_check(nums):
+        return False
+
+    sorted_nums = sort_nums(nums)
+
+    for i in range(len(sorted_nums)-1):
+        if isinstance(sorted_nums[i], str):
             break
-        if nums[i] < 0:
+        if sorted_nums[i] < 0:
             break
         if nums[i] == nums[i+1]:
             return nums[i]
 
     return False
-
-""" print(find_duplicate(["a", "b"])) """
-
