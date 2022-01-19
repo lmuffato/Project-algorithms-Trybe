d54a8a8344e45aa82b0c8fce4e30555f0c179701
nascjoao <oi@joaonasc.dev>
2022-01-19 18:11:05 -0300

    feat: study_schedule challenge

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for enter, exit in permanence_period:
+            if enter <= target_time <= exit:
+                counter += 1
+        return counter
+    except TypeError:
+        return None
