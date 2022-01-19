730f768ee53c1985a04a0abfd0052c3ad9f4e21a
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-01-19 10:23:19 -0300

    feat: mais um ponto do requisito 2 aprovado

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,10 +2,10 @@ def is_palindrome_recursive(word, low_index, high_index):
     size = len(word) // 2
     if word == '':
         return False
+    if low_index > size:
+        return True
     if word[low_index] != word[high_index]:
         return False
-    elif low_index <= size:
-        low_index += 1
-        high_index -= 1
-        is_palindrome_recursive(word, low_index, high_index)
+    if low_index <= size:
+        is_palindrome_recursive(word, low_index + 1, high_index -1)
     return True
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
     if (permanence_period or target_time) == '':
         return None
+    else:
+        best_hour = 0
+        for begin, end in permanence_period:
+            if (begin <= target_time) and (end >= target_time):
+                best_hour += 1
+    return best_hour
