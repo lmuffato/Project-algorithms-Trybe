73e739b21ef5d0b33083b3f74e3008b5591c6cba
Ana Ventura <anaventura1811@gmail.com>
2022-01-19 19:56:17 -0300

    começando o projeto

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    """ Faça o código aqui. 
+        recebe uma string ==> usa um split pra transformar cada letra em array
+        se possível, deixar tudo em minúscula ou tudo em maiúscula
+        reverter o array
+        usar o join pra retornar a string
+        
+    """
