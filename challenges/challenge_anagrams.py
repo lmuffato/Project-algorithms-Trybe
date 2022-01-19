d0d59ab9e97008c33e0916a0f9e40f03a504e4aa
Flavio Franco <flavio.win@gmail.com>
2022-01-19 13:18:52 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+""" def order(str):
+    for i in range(len(str)-1):
+        for j in range(i+1, len(str)):
+            if str[j] < str[i]:
+                aux1 = str[i]
+                aux2 = str[j]
+                str[i] = aux2
+                str[j] = aux1
+
+    return "".join(str) """
+
+
+def order(str):
+    ordered_string = ""
+    while len(str) > 0:
+        aux = min(str)
+        ordered_string += aux
+        str.remove(aux)
+
+    return ordered_string
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return order(list(first_string)) == order(list(second_string))
+
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,4 +7,3 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     else:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
