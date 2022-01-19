c856d7fffe5b3722fe847d0497370bed758aa96e
maricegalla <maricegalla@gmail.com>
2022-01-19 18:46:43 -0300

    feat: :sparkles: Requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
+def selection_sort(word):
+    arr = list(word)
+    for i in range(len(arr)):
+        minimum = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[minimum]:
+                minimum = j
+        arr[minimum], arr[i] = arr[i], arr[minimum]
+
+    return arr
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    if selection_sort(first_string) == selection_sort(second_string):
+        return True
+    if selection_sort(first_string) != selection_sort(second_string):
+        return False
