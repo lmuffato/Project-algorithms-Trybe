c96ba90b38596d5776115e2adce2dd9770133b03
mallmann02 <leomallmann020302@gmail.com>
2022-01-19 14:44:38 -0300

    style: identation

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,9 @@
 def study_schedule(permanence_period, target_time):
-  count = 0
-  if not target_time:
-    return None
-  for (first, second) in permanence_period:
-    try:
-      if target_time in range(first, second+1):
-        count += 1
-    except:
-      return None
-  return count
+    count = 0
+    for (first, second) in permanence_period:
+        try:
+            if int(target_time) in range(first, second+1):
+                count += 1
+        except TypeError:
+            return None
+    return count
