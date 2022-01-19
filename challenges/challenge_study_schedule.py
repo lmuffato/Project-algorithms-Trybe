6a8dc2c56c385caac281325fc02fc5b95465de6e
mallmann02 <leomallmann020302@gmail.com>
2022-01-19 16:13:38 -0300

    refact: replace "range" func to reduce exec time
    Replaced the "range" statement by a simple "<= x <=" expression

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     count = 0
     for (first, second) in permanence_period:
         try:
-            if int(target_time) in range(first, second+1):
+            if first <= int(target_time) <= second:
                 count += 1
         except TypeError:
             return None
