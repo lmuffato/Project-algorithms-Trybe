ddf4a4863c8dbdc66177efda3ab088930c8a3df9
iago <iago.pferreiravr@gmail.com>
2022-01-19 16:44:35 -0300

    refactor: Req4
    Arrumando o problema de complexidade
    
    Criando e implementando a funcao auxiliar is_duplicated

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,18 +1,22 @@
-def find_duplicate(nums):
-    if len(nums) == 1:
-        return False
-    nums.sort()
+def is_duplicated(nums):
     mid = 0
     start = 0
     end = len(nums)
     while start <= end:
         mid = (start + end) // 2
-        try:
-            if not isinstance(nums[mid], int) or nums[mid] < 0:
-                return False
-            if nums[mid] == nums[mid - 1] or nums[mid] == nums[mid + 1]:
-                return nums[mid]
-            else:
-                start += 1
-        except IndexError:
+        if not isinstance(nums[mid], int) or nums[mid] < 0:
             return False
+        if nums[mid] == nums[mid - 1] or nums[mid] == nums[mid + 1]:
+            return nums[mid]
+
+        start += 1
+
+
+def find_duplicate(nums):
+    if len(nums) == 1:
+        return False
+    nums.sort()
+    try:
+        return is_duplicated(nums)
+    except IndexError:
+        return False
