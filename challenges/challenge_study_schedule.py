16463a4c8d5ab9715d706d6e1d17550ccda3ee3f
Jodiel Briesemeister <jodielll29@gmail.com>
2022-01-19 19:40:31 -0300

    feat(req2): add is_palindrome_recursive, fix lint req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,12 @@
 def study_schedule(permanence_period, target_time):
 
-    if target_time == None:
+    if target_time is None:
         return target_time
 
     count = 0
 
     for a, b in permanence_period:
-        if a == None or b == None:
+        if a is None or b is None:
             return None
 
         if a <= target_time and b >= target_time:
