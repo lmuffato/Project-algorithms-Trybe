d1255ea48ae65d399605eccd48c8ebd58da43913
Gabriel <g.pereira.a.m@gmail.com>
2022-01-19 17:17:47 -0300

    fix(Req 1) - :sparkle: Corrigindo os erros do flake8 I

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,11 +2,11 @@ def study_schedule(permanence_period, target_time):
     ocorrence_target = 0
 
     if int != type(target_time):
-       return None
+        return None
 
     for key, element in permanence_period:
         if key is None or element is None:
-           return None
+            return None
         elif key <= target_time <= element:
-           ocorrence_target += 1
+            ocorrence_target += 1
     return ocorrence_target
