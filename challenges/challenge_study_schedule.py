505f62ea30b92f424cb6290a1ec63819d136d9c2
Rafael Geronimo <git_config_user_email>
2022-01-19 12:47:41 -0300

    feat: 1.4 | validar tempo schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    counter = 0
-    for period in permanence_period:
-        # https://stackoverflow.com/questions/31154372/what-is-the-best-way-to-check-if-a-tuple-has-any-empty-none-values-in-python
-        if not all(period):
-            return None
-        if period[0] <= target_time <= period[1]:
-            counter += 1
+    else:
+        counter = 0
+        for period in permanence_period:
+            if not all(period):
+                return None
+            if period[0] <= target_time <= period[-1]:
+                counter += 1
     return counter
