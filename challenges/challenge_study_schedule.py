5ad467936a899f8fcc0e7f1d0991adb2e03533b0
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2022-01-19 11:13:53 -0300

    fix flake8

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,7 +4,7 @@ def study_schedule(permanence_period, target_time):
     else:
         best_hour = 0
         for begin, end in permanence_period:
-            if begin == None or end == None:
+            if begin is None or end is None:
                 return None
             if (begin <= target_time) and (end >= target_time):
                 best_hour += 1
