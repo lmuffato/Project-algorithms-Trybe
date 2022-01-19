8325454b9e686d5acd24f7815a6e46a360636af8
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-19 17:27:33 -0300

    Requisito 1 - verificando tupla e target time

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. - Começando projeto - teste """
+    """ Faça o código aqui. """
+    # permanence_period -[(hora entrada, hora saida)]
+    # print(permanence_period)
+    # https://stackoverflow.com/questions/58656740/python-check-if-tuple-contain-only-integers-with-while
+    # https://www.w3schools.com/python/ref_func_isinstance.asp
+    # quant_students = 0
+
+    if target_time is None:
+        return None
+
+    for period in permanence_period:
+        if isinstance(period, int):
+            return None
