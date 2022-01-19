d05379359af20a790679d4a706494de5f6df4d44
Gui-lira <gui.lira.martins@gmail.com>
2022-01-19 16:35:49 -0300

    2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if len(word) < 2:
+        return True
+    future_index = low_index + 1
+    if future_index == len(word):
+        return word[low_index] == word[high_index]
+    return (
+        word[low_index] == word[high_index] and is_palindrome_recursive(
+            word, future_index, high_index - 1))
