001aa5ebe58201d7b4e9b127260de3fef662856a
Bruno Mendes <mendessbruno@gmail.com>
2022-01-19 19:44:31 -0300

    tentativa resoluçao exercico 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+from typing import Counter
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == 0:
+        return None
+    else:
+        counter = 0
+        for element in permanence_period:
+            a = element[0]
+            b = element[1]
+            if a < 0 or b < 0:
+                return None
+            else:
+                if a <= target_time and b >= target_time:
+                    counter += 1
+        return counter
