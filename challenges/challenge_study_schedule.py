5e8dd0cbb144643c01245fb64b7fbfd69fc304c4
Rafael Geronimo <git_config_user_email>
2022-01-19 11:51:44 -0300

    feat: 1.2 | validar permanence periods com entradas inválidas

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,12 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
     for period in permanence_period:
+        if (type(period[0])) != int or (type(period[1])) != int:
+            return None
         if target_time >= period[0] and target_time <= period[1]:
             counter += 1
     return counter
+
+# permanence_periods = [(4, "0"), (2, 4)]
+# target_time = 4
+# print(study_schedule(permanence_periods, target_time))
