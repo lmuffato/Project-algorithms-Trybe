7f08bc1f2ee9852dff9875d40317de7254158888
Rafael Geronimo <git_config_user_email>
2022-01-19 11:27:09 -0300

    feat: 1.1 | validar melhor horário com sucesso

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui """
+    counter = 0
+    for period in permanence_period:
+        if target_time >= period[0] and target_time <= period[1]:
+            counter += 1
+    return counter
