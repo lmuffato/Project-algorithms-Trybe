09bc8b2c329f09e91772f727b7e0d892f2922a57
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-01-19 15:29:33 -0300

    feat: req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    count = 0
+    for time in permanence_period:
+        if time[0] is None or time[1] is None:
+            return None
+        if time[0] <= target_time and time[1] >= target_time:
+            count += 1
+    return count
