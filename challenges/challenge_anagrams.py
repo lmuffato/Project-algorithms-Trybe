118ce9ad73892fcbce1e591914562e1ed2d5286f
nascjoao <oi@joaonasc.dev>
2022-01-19 18:54:15 -0300

    feat: anagrams challenge

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    second_string_letters = list(second_string)
+    first_string_letters = list(first_string)
+    if len(first_string) != len(second_string):
+        return False
+    for letter in first_string_letters:
+        if letter not in second_string_letters:
+            return False
+        second_string_letters.remove(letter)
+    return True
