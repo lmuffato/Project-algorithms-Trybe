8183b53d22dfe24550f91d2518f2773647c08d59
Renzo Sevilha <sevilharenzo@gmail.com>
2022-01-19 20:53:18 -0300

    feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,16 @@
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+target_time = 5
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        max = 0
+
+        for period in permanence_period:
+            if period[0] is target_time or period[1] is target_time:
+                max += 1
+
+        return max
+
+    except TypeError:
+        return None
