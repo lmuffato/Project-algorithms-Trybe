cc91b64548a31a86c44602b7e2072c8931d49302
MarcioFujita <hanker78@gmail.com>
2022-01-19 14:40:13 -0300

    feat: Req 1, try 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    max_value = 0
+    for entrance, leave in permanence_period:
+        if entrance <= target_time:
+            if target_time <= leave:
+                max_value += 1
+        else:
+            return None
+    return max_value
