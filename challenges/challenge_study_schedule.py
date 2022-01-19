51f2231c62be12cbb975d042f751cb0610946052
Rafael Geronimo <git_config_user_email>
2022-01-19 12:32:31 -0300

    feat: 1.4 | validar tempo schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
         return None
     counter = 0
     for period in permanence_period:
-        if (type(period[0])) != int or (type(period[1])) != int:
+        if not all(period):
             return None
         if target_time >= period[0] and target_time <= period[1]:
             counter += 1
