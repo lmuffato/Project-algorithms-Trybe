d9a7ff78ae9b4cc77a32595abdd2c6fb7cd52107
Ana Ventura <anaventura1811@gmail.com>
2022-01-19 20:34:03 -0300

    feat: implementa algoritmo palindromes recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,27 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. 
-        recebe uma string ==> usa um split pra transformar cada letra em array
-        se possível, deixar tudo em minúscula ou tudo em maiúscula
-        reverter o array
-        usar o join pra retornar a string
-        
-    """
+    test = []
+    word_reversed = ''
+    if word:
+        word_test = list(word)
+        new_high_index = high_index
+        new_low_index = low_index
+        for char in word_test:
+            if char == word_test[high_index] and high_index != -1:
+                test.append(char)
+                low_index += 1
+                high_index -= 1
+                new_high_index -= 1
+                new_low_index += 1
+                # print(low_index)
+                # print(high_index)
+                # print(new_low_index)
+                # print(new_high_index)
+                is_palindrome_recursive(word, new_low_index, new_high_index)
+            # low_index += 1
+            # high_index -= 1
+        # print(word_reversed)
+        word_reversed = "".join(test)
+        if word == word_reversed:
+            return True
+        return False
+    return False
