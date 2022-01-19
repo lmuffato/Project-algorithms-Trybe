78521732f8e531e47e9dd8a3e4f15fd4d1794387
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-19 15:29:56 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    else:
+        iterator = 0
+        for run_time in permanence_period:
+            if run_time[0] is None or run_time[1] is None:
+                return None
+            if run_time[0] <= target_time and run_time[1] >= target_time:
+                iterator += 1
+    return iterator
