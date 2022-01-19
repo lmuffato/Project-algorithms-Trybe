2aeeb6287c55603e98fbda6145d37f30e74b33c7
Bruno Mendes <mendessbruno@gmail.com>
2022-01-19 20:22:51 -0300

    resolvido desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,15 @@
+# validação de inteiro
+# https://www.delftstack.com/howto/python/python-check-variable-type/#:~:text=variable%20in%20Python.-,Use%20the%20type()%20Function%20to%20Check%20Variable%20Type%20in,return%20the%20variable%20data%20type.
+
+
 def study_schedule(permanence_period, target_time):
-    if target_time == 0:
-        return None
-    else:
+    try:
         counter = 0
         for element in permanence_period:
             a = element[0]
             b = element[1]
-            if a < 0 or b < 0:
-                return None
-            else:
-                if a <= target_time and b >= target_time:
-                    counter += 1
+            if a <= target_time and b >= target_time:
+                counter += 1
         return counter
+    except TypeError:
+        return None
