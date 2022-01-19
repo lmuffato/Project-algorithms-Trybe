1f76454866726b7d88a06d9689e2c76b455a509c
maricegalla <maricegalla@gmail.com>
2022-01-19 18:57:23 -0300

    feat: :sparkles: Requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,7 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if word == word[::-1]:
+        return True
+    else:
+        return False
