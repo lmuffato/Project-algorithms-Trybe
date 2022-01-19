8f9fa33a2f5fab8d280a1e53d911602b14dc5055
Rafael Geronimo <git_config_user_email>
2022-01-19 19:27:45 -0300

    feat: 3 | Anagramas (Algoritmo de ordernação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def found_letters(first_string, second_string):
+    letter_found_position = []
+    for i in first_string:
+        for j_index, j in enumerate(second_string):
+            if (i == j) and (j_index not in letter_found_position):
+                letter_found_position.append(j_index)
+                break
+    if len(letter_found_position) == len(first_string):
+        return True
+    return False
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    return found_letters(first_string, second_string)
