2c527ce8c566e1381e662ec24adb0e95e11e86ed
Rafael Geronimo <git_config_user_email>
2022-01-19 11:59:12 -0300

    feat: 1.3 | validar `target_time` com valor vazio

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,6 @@
 def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
     counter = 0
     for period in permanence_period:
         if (type(period[0])) != int or (type(period[1])) != int:
@@ -6,7 +8,3 @@ def study_schedule(permanence_period, target_time):
         if target_time >= period[0] and target_time <= period[1]:
             counter += 1
     return counter
-
-# permanence_periods = [(4, "0"), (2, 4)]
-# target_time = 4
-# print(study_schedule(permanence_periods, target_time))
