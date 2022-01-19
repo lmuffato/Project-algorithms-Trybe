d532e4b8cffb7f5da6457443cd5e23b007bc1e35
mallmann02 <leomallmann020302@gmail.com>
2022-01-19 14:18:18 -0300

    feat: req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+  count = 0
+  if not target_time:
+    return None
+  for (first, second) in permanence_period:
+    try:
+      if target_time in range(first, second+1):
+        count += 1
+    except:
+      return None
+  return count
