404f4d7b7d9633c80b7801b6892caeb8f1c74ef7
Gui-lira <gui.lira.martins@gmail.com>
2022-01-19 15:33:42 -0300

    1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
+def validate_tuples(tpList):
+    return type(tpList[0]) == int and type(tpList[1]) == int
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    numberStudents = 0
+    if target_time is None:
+        return None
+    for tp in permanence_period:
+        if not all(tp):
+            return None
+        if tp[0] <= target_time and tp[1] >= target_time:
+            numberStudents += 1
+    return numberStudents
