f7d8ddb9b299cbb2b0d66af5257db95c986ed355
iago <iago.pferreiravr@gmail.com>
2022-01-19 14:55:36 -0300

    feat: Req2
    Implementando a função is_palindrome_recursive no arquivo challenge_palindrome_recursive
    
    Em conjunto com a função auxiliar reverse que inverte os itens de um array

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
+def reverse(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reverse(list[1:]) + [list[0]]
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    reversed = ''.join(reverse(list(word)))
+    normal = ''.join(list(word))
+    if normal == reversed:
+        return True
+    return False
