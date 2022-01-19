a6435e7d447f4dba46713d9ab1b4f0537599ada4
iago <iago.pferreiravr@gmail.com>
2022-01-19 13:52:44 -0300

    feat: Req1
    Implementando a função study_schedule no arquivo challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    counter = 0
+    for check_in, check_out in permanence_period:
+        if not isinstance(check_in, int) or not isinstance(check_out, int):
+            return None
+        if check_in <= target_time <= check_out:
+            counter += 1
+
+    return counter
