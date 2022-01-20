0845cce924101d6b03a13e663de9eee2c1194dbe
Felipe Muller <felipeavmuller@gmail.com>
2022-01-20 18:19:05 -0300

    Requisito 2 - Retorna se e palindromo de forma recursiva

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if (
+        (low_index == high_index
+            and word[low_index] == word[high_index])
+        or high_index == 1
+    ):
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index+1, high_index-1)
+    else:
+        return False
