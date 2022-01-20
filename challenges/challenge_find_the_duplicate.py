1b136d492cc7950cc5e995c2c4385d0d00937625
Ana Ventura <anaventura1811@gmail.com>
2022-01-20 01:18:06 -0300

    feat: implementa req. bônus, find_the_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums_list = []
+    duplicate = []
+    if nums:
+        for index in range(len(nums)):
+            if nums[index] not in nums_list:
+                nums_list.append(nums[index])
+            else:
+                duplicate.append(nums[index])
+        for num in duplicate:
+             if len(duplicate) > 0 and num > -1:
+                return num
+             return False
+    return False
