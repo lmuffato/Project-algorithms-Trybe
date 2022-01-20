2f59ac51bfdb4cc3478b14d15365d4081bfac32e
MaxRudim <max.hermsdorff@gmail.com>
2022-01-19 21:18:18 -0300

    Requisito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    counter = 0
+    for first, second in permanence_period:
+        if first and second is None:
+            return None
+        if first <= target_time and target_time <= second:
+            counter += 1
+    return counter
