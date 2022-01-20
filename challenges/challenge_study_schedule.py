ec7d9f7df7952ee01d8b7a5a4b73757f8ca601d3
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-01-20 15:26:36 -0300

    FEAT:Req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-# iniciando o projeto
+    if target_time is None:
+        return None
+    sum = 0
+    for period in permanence_period:
+        if period[0] is None or period[1] is None:
+            return None
+        if period[0] <= target_time <= period[1]:
+            sum += 1
+
+    return sum
