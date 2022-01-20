6547d309044cd4198ca29d98785ae132bf9e98ff
Lucas Lara <lucasml@live.com>
2022-01-20 11:41:56 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    counter = 0
+    try:
+        for time in permanence_period:
+            if target_time >= time[0] and target_time <= time[1]:
+                counter += 1
+    except TypeError:
+        return None
+    return counter
