5981335881e37ab4afc3dccc4a953db125a6b114
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-20 09:53:27 -0300

    Feat: Implementa funçao 'is_palindrome_recursive'

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    _break = len(word) // 2
+    if word == "" or word[low_index] != word[high_index]:
+        return False
+    if low_index == _break:
+        return True
+    if low_index <= _break:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
