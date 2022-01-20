7b94c9a5cf3d3a4fb1de3659f04c0e568ebee74b
Ana Ventura <anaventura1811@gmail.com>
2022-01-20 13:00:50 -0300

    fix: organiza comentários de referência

---
+++ challenges/anagrams_challenge_alternative_answers.py
@@ -0,0 +1,43 @@
+# Anotações para referências futuras --
+# Duas abordagens para resolver esse problema:
+
+# --> Abordagem 1 - usando um algoritmo de ordenação e algoritmo de recursão:
+# Transformar as duas strings em duas listas ordenada em ordem alfabética
+
+#  Funcionamento do quicksort:
+# Se não for passado o parâmetro lista, retorna um array vazio.
+#  continua: retornando o item para cada item na lista,
+# Se há retorna o item para cada item na lista,
+# excluído o primeiro elemento da lista, chama o quicksort se o item for menor
+# que o primeiro elemento.
+#  Adiciona-se novamente o primeiro item da lista.
+# Depois, excluído o primeiro elemento da lista, chama novamente o quicksort
+#  se o item for >= ao que está no índice zero do array
+
+
+def quicksort(lista_or_str):
+    if not lista_or_str:
+        return []
+    return (
+        quicksort(
+            [item for item in lista_or_str[1:] if item < lista_or_str[0]])
+        + [lista_or_str[0]] +
+        quicksort(
+            [item for item in lista_or_str[1:] if item >= lista_or_str[0]]))
+# Source - algoritmo quicksort:
+# https://www.delftstack.com/pt/howto/python/sort-list-alphabetically/
+
+# Depois iterar sobre a lista e verificar se todas as posições
+# do array possuem valores iguais
+# e tiverem o mesmo tamanho, é porque são anagramas
+# Se tiver alguma letra diferente é false
+
+
+def is_anagram(first_string, second_string):
+    if first_string and second_string:
+        if len(first_string) == len(second_string):
+            if quicksort(first_string) == quicksort(second_string):
+                return True
+            return False
+        return False
+    return False
--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,70 +1,27 @@
 from string import ascii_lowercase as letters
 
-# Anotações para referências futuras --
-# Duas abordagens para resolver esse problema:
-
-# --> Abordagem 1 - usando um algoritmo de ordenação e algoritmo de recursão:
-# Transformar as duas strings em duas listas ordenada em ordem alfabética
-
-#  Funcionamento do quicksort:
-# Se não for passado o parâmetro lista, retorna um array vazio.
-#  continua: retornando o item para cada item na lista,
-# Se há retorna o item para cada item na lista,
-# excluído o primeiro elemento da lista, chama o quicksort se o item for menor
-# que o primeiro elemento.
-#  Adiciona-se novamente o primeiro item da lista.
-# Depois, excluído o primeiro elemento da lista, chama novamente o quicksort
-#  se o item for >= ao que está no índice zero do array
-
-
-def quicksort(lista_or_str):
-    if not lista_or_str:
-        return []
-    return (
-        quicksort(
-            [item for item in lista_or_str[1:] if item < lista_or_str[0]])
-        + [lista_or_str[0]] +
-        quicksort(
-            [item for item in lista_or_str[1:] if item >= lista_or_str[0]]))
-# Source - algoritmo quicksort:
-# https://www.delftstack.com/pt/howto/python/sort-list-alphabetically/
-
-# Depois iterar sobre a lista e verificar se todas as posições
-# do array possuem valores iguais
-# e tiverem o mesmo tamanho, é porque são anagramas
-# Se tiver alguma letra diferente é false
-
-# def is_anagram(first_string, second_string):
-#     if first_string and second_string:
-#         if len(first_string) == len(second_string):
-#               if quicksort(first_string) == quicksort(second_string):
-#                   return True
-#               return False
-#         return False
-#     return False
-
 # --> Abordagem 2 - utilizando fórmula matemática para números primos:
-    # Ter uma string ou array contendo todas as letras
-    # do alfabeto já ordenadas
-    #  (aqui usamos o módulo string.ascii_lowercase)
-    # Verificar se as strings possuem o mesmo número de caracteres
-    # Então, para cada índice na lista de letras e para cada letra
-    # na string testada,
-    # quando a letra coincidir com a sua correspondente na
-    # lista de letras do alfabeto,
-    # convertemos o índice daquela letra em número primo e
-    # colocamos este número primo dentro
-    # de uma lista de números.
-    # Iteramos a lista de números primos gerada e
-    # toda vez que o número da lista de números não for None,
-    # vamos somar (ou multiplicar, os dois jeitos funcionam)
-    # este número pelo valor atual guardado
-    # nas variáveis de soma
-    # (sum_one para a primeira string e sum_two para a segunda)
-    # se os valores de sum_one e sum_two forem iguais,
-    # as strings são anagramas (retorna True),
-    # se o valor não for o mesmo é porque
-    # as strings não são anagramas (retorna False)
+# Ter uma string ou array contendo todas as letras
+# do alfabeto já ordenadas
+#  (aqui usamos o módulo string.ascii_lowercase)
+# Verificar se as strings possuem o mesmo número de caracteres
+# Então, para cada índice na lista de letras e para cada letra
+# na string testada,
+# quando a letra coincidir com a sua correspondente na
+# lista de letras do alfabeto,
+# convertemos o índice daquela letra em número primo e
+# colocamos este número primo dentro
+# de uma lista de números.
+# Iteramos a lista de números primos gerada e
+# toda vez que o número da lista de números não for None,
+# vamos somar (ou multiplicar, os dois jeitos funcionam)
+# este número pelo valor atual guardado
+# nas variáveis de soma
+# (sum_one para a primeira string e sum_two para a segunda)
+# se os valores de sum_one e sum_two forem iguais,
+# as strings são anagramas (retorna True),
+# se o valor não for o mesmo é porque
+# as strings não são anagramas (retorna False)
 
 
 def get_num_prime(num):
@@ -79,7 +36,7 @@ def convert_letter_to_prime(letter):
     for index in range(0, len(letters)):
         for char in letter:
             if char == letters[index]:
-                num = get_num_prime(index)
+                num = get_num_prime(index + 1)
                 primes_nums.append(num)
     return primes_nums
 
