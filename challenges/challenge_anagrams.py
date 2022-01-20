8c41d4c13d81a6bb644b3159fc48b29ce9e0cccc
giovanni-maldonado <giovannitadeu08@gmail.com>
2022-01-20 16:05:31 -0300

    feat: requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+def letter_counter(string):
+    dict = {}
+    for letter in string:
+        dict[letter] = dict.setdefault(letter, 0) + 1
+    return dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui """
+    if (first_string or second_string) == '':
+        return False
+    else:
+        str1 = letter_counter(first_string)
+        str2 = letter_counter(second_string)
+        return str1 == str2
