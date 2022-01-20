89e93f9efaf2f11294ce083e125d6bab460ab55e
Gabriel <g.pereira.a.m@gmail.com>
2022-01-20 13:21:31 -0300

    fix(Req 3) - :sparkle Corrigindo os erros do flake8 III

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -8,13 +8,13 @@ def aux_check(left, right, merged):
         else:
             merged[left_cursor + right_cursor] = right[right_cursor]
             right_cursor += 1
-    
+
     for left_cursor in range(left_cursor, len(left)):
         merged[left_cursor + right_cursor] = left[left_cursor]
-    
+
     for right_cursor in range(right_cursor, len(right)):
         merged[left_cursor + right_cursor] = right[right_cursor]
-    
+
     return merged
 
 
@@ -26,17 +26,7 @@ def anagrama_check(array):
 
     left, right = anagrama_check(array[:meio]), anagrama_check(array[meio:])
     return aux_check(left, right, array.copy())
-    
+
 
 def is_anagram(first_string, second_string):
     return anagrama_check(list(first_string)) == anagrama_check(list(second_string))
-
-# first_string = "pato"
-# second_string = "topa"
-# iteracoes = 0
-# print(len(first_string) - iteracoes - 1)
-# print(list_words(first_string))
-# print(list_words(second_string))
-# print(list(first_array))
-# print(is_anagram("pedra", ""))
-# print(first_string[0].lower())
