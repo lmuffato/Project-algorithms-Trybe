eb09aa1863e295400594e8801705c7092161589b
Micaela Alves <micaela.alves93@gmail.com>
2022-01-20 20:52:01 -0300

    feat - requisito 1 anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+def sort_string(string):
+    new_string = list(string)
+    for i in range(len(string)):
+        cur_value = string[i]
+        cur_position = i
+        while cur_position > 0 and new_string[cur_position - 1] > cur_value:
+            new_string[cur_position] = new_string[cur_position - 1]
+            cur_position = cur_position - 1
+
+        new_string[cur_position] = cur_value
+    return new_string
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. !!"""
+    if len(first_string) != len(second_string):
+        return False
+    else:
+        first_string = sort_string(first_string)
+        second_string = sort_string(second_string)
+
+    for i in range(len(first_string)):
+        if first_string[i] != second_string[i]:
+            return False
+    return True
