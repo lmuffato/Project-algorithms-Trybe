7df8639fca382c033bf33fb870565edef3a86f86
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-01-20 16:53:46 -0400

    requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    count = 0
+
+    for student in permanence_period:
+        if not student[0] or not student[1]:
+            return None
+        if student[0] <= target_time <= student[1]:
+            count += 1
+
+    return count
