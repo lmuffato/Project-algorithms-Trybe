4a3983994a8b22df853448fe0993761a229998b3
Viegas <mvrdgs@gmail.com>
2022-01-20 10:43:37 -0300

    requisito 1: alterado verificacao type == int para not

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if (type(target_time) == int) is False:
+    if not target_time:
         return None
     counter = 0
     for period in permanence_period:
-        if type(period[0]) != int or type(period[1]) != int:
+        if not period[0] or not period[1]:
             return None
         if target_time >= period[0] and target_time <= period[1]:
             counter += 1
