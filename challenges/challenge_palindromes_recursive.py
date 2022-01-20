65a7055913f7bce4413ab239b5a37fe35f8243cb
Viegas <mvrdgs@gmail.com>
2022-01-20 11:08:51 -0300

    exercicio 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    else:
+        if low_index < high_index:
+            if word[low_index] == word[high_index]:
+                return is_palindrome_recursive(
+                    word, low_index + 1, high_index - 1
+                )
+            else:
+                return False
+        else:
+            return True
