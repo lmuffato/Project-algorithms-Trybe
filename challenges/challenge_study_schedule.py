27450fa0a7cc3e2b7ab06d5d426c65272fe6587b
Robertson <robertson.nunes@gmail.com>
2022-01-20 15:10:31 -0300

    feat: req1 study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if (target_time is None):
+        return None
+    else:
+        count = 0
+        for login, logout in permanence_period:
+            if login is None or logout is None:
+                return None
+            if (login <= target_time) and (logout >= target_time):
+                count += 1
+    return count
