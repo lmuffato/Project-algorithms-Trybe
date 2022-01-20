de93515ab6d7b4d3c3ca0d6c96b18443caa184de
Felipe Flores <felipefloresvendas@gmail.com>
2022-01-20 09:29:39 -0300

    feat: requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+# src: https://panda.ime.usp.br/panda/static/pythonds_pt/
+# 02-AnaliseDeAlgoritmos/ExemploDeDeteccaoDeAnagramas.html
+def check(word):
+    stored_strings = ""
+    while len(word) > 0:
+        first_letter_word = min(word)
+        stored_strings += first_letter_word
+        word.remove(first_letter_word)
+
+    return stored_strings
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    return check(list(first_string)) == check(list(second_string))
