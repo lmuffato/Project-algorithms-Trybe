f359a097ea39c8f4a15c7091e473cc073f83d5a5
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-20 18:08:24 -0300

    requisito 03 (testando tempo de resposta)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def sort_array(param):
+    array = list(param)
+    for i in range(len(array)):
+        minimo = i
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimo]:
+                minimo = j
+        array[minimo], array[i] = array[i], array[minimo]
+    return "".join(array)
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    elif len(first_string) != len(second_string):
+        return False
+    return sort_array(first_string) == sort_array(second_string)
