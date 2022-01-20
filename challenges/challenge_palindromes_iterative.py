d43f22e1e04168adb5ea817c228ca3f7358efe12
Viegas <mvrdgs@gmail.com>
2022-01-20 11:53:44 -0300

    requisito 5: utilizado string para montar a palavra invertida ao inves de array

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,8 +1,8 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    reverseWord = []
-    for letter in word:
-        reverseWord.insert(0, letter)
+    reverseWord = ''
+    for letter in word[::-1]:
+        reverseWord += letter
 
-    return "".join(reverseWord) == word
+    return reverseWord == word
