ad1e6618885343ed8f26bbd1d8db276e3eccb1b9
Felipe Muller <felipeavmuller@gmail.com>
2022-01-20 19:09:50 -0300

    Requisito 3 - Descobre se e anagrama

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+# Remover um character de uma string
+# https://stackoverflow.com/questions/3559559/how-to-delete-a-character-from-a-string-using-python
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui.. """
+    if (
+        not first_string 
+        or not second_string
+        or len(first_string) != len(second_string)
+    ):
+        return False
+
+    for first_index in range(len(first_string)):
+        for second_index in range(len(second_string)):
+            if first_string[first_index] == second_string[second_index]:
+                second_string = second_string[:second_index] + second_string[(second_index+1):]
+                break
+    if len(second_string) > 0:
+        return False
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,8 +2,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
     if (
-        (low_index == high_index
-            and word[low_index] == word[high_index])
+        (low_index == high_index and word[low_index] == word[high_index])
         or high_index == 1
     ):
         return True
