bda81c4b30f791aea3198fe39d1c7a76bca95f38
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-20 14:07:52 -0300

    feat: start second requirement

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    backwards_word = backwards(word)
+    if backwards_word == word:
+        return True
+    else:
+        return False
+
+
+def backwards(word):
+    if len(word) == 1:
+        return word
+    first_letter = word[:1]
+    rest_backwards = backwards(word[1:])
+    return rest_backwards + first_letter
