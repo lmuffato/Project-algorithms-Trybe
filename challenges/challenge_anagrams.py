8f8b85249469277a95d4d64e1c8ac97c0772d8ab
MarcioFujita <hanker78@gmail.com>
2022-01-20 16:24:11 -0300

    feat: Req 3 Try 1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,36 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    list_first_string = list(first_string)
+    list_second_string = list(second_string)
+    return merge_sort(list_first_string) == merge_sort(list_second_string)
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor = 0
+    right_cursor = 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
