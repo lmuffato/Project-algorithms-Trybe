f6a679847fec7ba184f28e9e3fc3acd6fe9d03f5
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-20 13:41:21 -0300

    feat: start first requirement

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    # Go!
+    if target_time == None:
+        return None
+    counter = 0
+    for period in permanence_period:
+        if type(period[0]) == int and type(period[1]) == int:
+            if period[0] <= target_time <= period[1]:
+                counter += 1
+        else:
+            return None
+            break
+    return counter
