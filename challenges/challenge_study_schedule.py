14b1ebbaeb8cb190a3727eddce2bf36b63e022a0
Felipe Muller <felipeavmuller@gmail.com>
2022-01-20 16:51:53 -0300

    Requisito 1 - busca quantidade de alunos online em determinado horario

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    result = 0
+    if not target_time:
+        return None
+    for period in permanence_period:
+        if None in period:
+            return None
+        elif period[0] <= target_time <= period[1]:
+            result += 1
+    return result
