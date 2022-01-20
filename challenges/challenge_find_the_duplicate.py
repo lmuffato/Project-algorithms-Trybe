98b95ba03bda412ce3a835e0c3ce6be6d68e3915
Ana Ventura <anaventura1811@gmail.com>
2022-01-20 01:34:04 -0300

    refatoração de find_duplicate para reduzir complexidade do algoritmo

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,14 +1,24 @@
-def find_duplicate(nums):
+def extract_duplicate(nums):
     nums_list = []
     duplicate = []
+    for index in range(len(nums)):
+        if nums[index] not in nums_list:
+            nums_list.append(nums[index])
+        else:
+            duplicate.append(nums[index])
+    return duplicate
+
+
+def find_duplicate(nums):
+    duplicate = []
     if nums:
-        for index in range(len(nums)):
-            if nums[index] not in nums_list:
-                nums_list.append(nums[index])
-            else:
-                duplicate.append(nums[index])
+        duplicate = extract_duplicate(nums)
         for num in duplicate:
-             if len(duplicate) > 0 and num > -1:
+            if len(duplicate) > 0 and num > -1:
                 return num
-             return False
+            return False
     return False
+
+# Para montar a lógica deste algoritmo
+# foram consultadas as seguintes fontes:
+# https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
