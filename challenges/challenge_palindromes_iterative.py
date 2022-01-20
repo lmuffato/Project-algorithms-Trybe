e2de62d3dd26c6c166ab9100c331b280d5090f3a
Viegas <mvrdgs@gmail.com>
2022-01-20 11:43:08 -0300

    requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    reverseWord = []
+    for letter in word:
+        reverseWord.insert(0, letter)
+
+    return "".join(reverseWord) == word
