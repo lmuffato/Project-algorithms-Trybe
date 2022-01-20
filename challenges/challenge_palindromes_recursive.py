a1f59c3187c4eacdd85cbd268d8a7dd4a31696d7
Lucas Lara <lucasml@live.com>
2022-01-20 12:32:35 -0300

    Requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    else:
+        return word[0] == word[1] and is_palindrome_recursive(
+            word[1:-1], low_index, high_index
+        )
