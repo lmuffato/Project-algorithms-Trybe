34762da98eb40cdd6fce833ee575ca58011b2cf0
Ana Ventura <anaventura1811@gmail.com>
2022-01-20 03:05:15 -0300

    feat: último req. bônus parcialmente implementado

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,16 @@
+
+def test_each_char_in_word(word):
+    key = len(word) - 1
+    reverse_word = []
+    while key > -1:
+        reverse_word.append(word[key])
+        key -= 1
+    return "".join(reverse_word)
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word:
+        if word == test_each_char_in_word(word):
+            return True
+        return False
+    return False
