42548700603c0bbcc2a749b70175d2879fb62863
Micaela Alves <micaela.alves93@gmail.com>
2022-01-20 18:09:04 -0300

    feat - requisito 5 palidrome iterativa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    high_index = len(word) - 1
+
+    for i in range(len(word)):
+        if i == high_index:
+            return True
+        if word[i] != word[high_index]:
+            return False
+        high_index -= 1
+    return True
