f2711b7ca2cda7feb1d11acc010ce1ee9dd876ec
Hudson Carlos <hc88806@gmail.com>
2022-01-20 17:41:57 -0400

    challenge study done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    num = 0
+    for hrs in permanence_period:
+        if None in hrs or target_time is None:
+            return None
+
+        if hrs[0] <= target_time <=hrs[1]:
+            num += 1
+    return num
