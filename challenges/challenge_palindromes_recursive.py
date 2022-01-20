550bd4131e58511af7e0ddefc25a35b9d76462e9
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-20 19:09:18 -0300

    Req 3: Palindro recursivo - uma tarde inteira pra fazer issogit add .!

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if word:
+        if high_index == 0 or high_index == 1:
+            return True
+        elif word[low_index] == word[high_index]:
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        else:
+            return False
+    else:
+        return False
