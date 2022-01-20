d7f71b86516971a45f413fcfc727ae5e04fdd195
Mathaus Annibal <mathausannibal@gmail.com>
2022-01-20 16:24:56 -0300

    adiciona requisito 2 - tentativa 1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,8 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    """Faça o código aqui."""
+    if word == "":
+        return False
+    elif word[::-1] == word:
+        return True
+    else:
+        return False
