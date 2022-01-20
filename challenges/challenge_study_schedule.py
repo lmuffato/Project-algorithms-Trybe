e55deb6d68beb20ca7ac2f70c6fe819b7013a1ed
Lucas André <lucas.cancado89@gmail.com>
2022-01-20 20:21:17 -0300

    req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for p in permanence_period:
+        if type(p[0]) != int or type(p[1]) != int or type(target_time) != int:
+            return None
+        index = p[0]
+        while index <= p[1]:
+            if index == target_time:
+                count += 1
+            index += 1
+    return count
