26a2a8e947a4c300f6938f124e5008629399472a
Renzo Sevilha <sevilharenzo@gmail.com>
2022-01-19 21:34:35 -0300

    feat: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+
+    if high_index == int(len(word) / 2 - 1):
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
