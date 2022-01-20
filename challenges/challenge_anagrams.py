1b43e33cdf45ba4367ab1622e8a0f01646cedb80
Felipe Muller <felipeavmuller@gmail.com>
2022-01-20 19:18:51 -0300

    FIX Flake8 - refatora funcoes para diminuir complexidade

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,19 +2,33 @@
 # https://stackoverflow.com/questions/3559559/how-to-delete-a-character-from-a-string-using-python
 
 
-def is_anagram(first_string, second_string):
+def validate_entries(first, second):
     if (
-        not first_string 
-        or not second_string
-        or len(first_string) != len(second_string)
+        not first
+        or not second
+        or len(first) != len(second)
     ):
         return False
+    return True
 
+
+def anagram_check(first_string, second_string):
     for first_index in range(len(first_string)):
         for second_index in range(len(second_string)):
             if first_string[first_index] == second_string[second_index]:
-                second_string = second_string[:second_index] + second_string[(second_index+1):]
+                second_string = (
+                    second_string[:second_index] +
+                    second_string[(second_index+1):]
+                )
                 break
     if len(second_string) > 0:
+        return False
+    return True
+
+
+def is_anagram(first_string, second_string):
+    validate = validate_entries(first_string, second_string)
+    anagram = anagram_check(first_string, second_string)
+    if not validate or not anagram:
         return False
     return True
