804c95e4a206c02ce3c145ce13e6034680bd6e99
Carlos Sá <carlos25xrm@gmail.com>
2022-01-20 16:13:37 -0300

    1 - Número de estudantes estudando no mesmo horário (Algoritmo de busca)

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    # Consegui com ajuda do Leonardo Mallmann
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+
+    students = 0
+    for time in permanence_period:
+        if (time[0] <= target_time <= time[1]):
+            students += 1
+    return students
