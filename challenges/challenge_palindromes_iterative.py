23699b1263eb077a01bd3d9517d06274c27ac0f8
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-19 22:40:07 -0300

    Req 2: palindromo iterativo

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    # https://pt.stackoverflow.com/questions/401489/inverter-as-palavras-de-uma-frase-mantendo-a-ordem-delas-na-frase
+    # count_1 = 0
+    # count_2 = len(word) - 1
+    word_aux = word[::-1]
+    if not word:
+        return False
+    elif word == word_aux:
+        return True
+    else:
+        return False
