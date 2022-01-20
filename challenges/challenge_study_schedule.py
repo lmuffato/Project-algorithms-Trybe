788e25c350d44086502f04546b983c834f74f7b6
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-20 13:44:12 -0300

    style: fix cond is None

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    if target_time == None:
+    if target_time is None:
         return None
     counter = 0
     for period in permanence_period:
