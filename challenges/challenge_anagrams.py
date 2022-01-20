f170619f0c15d7ec97b0bf0b70f43bee27f862f1
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2022-01-20 16:05:39 -0300

    feat: req03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
+def letter_counter(string):
+    dict = {}
+    for letter in string:
+        dict[letter] = dict.setdefault(letter, 0) + 1
+    return dict
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui aa. """
+    if first_string == '' or second_string == '':
+        return False
+    strOne = letter_counter(first_string)
+    strTwo = letter_counter(second_string)
+    return strOne == strTwo
