28c59dc85405229e8ac23592cbd5d67171db0082
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-20 16:53:24 -0300

    fix: complexity

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
-    if target_time is None:
-        return None
     counter = 0
-
-    for period in permanence_period:
-        try:
+    try:
+        for period in permanence_period:
             if period[0] <= target_time <= period[1]:
                 counter += 1
-        except TypeError:
-            return None
+    except TypeError:
+        return None
+
     return counter
