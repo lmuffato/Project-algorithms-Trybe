5f36be2b9f2a8622d53132affe7bb4c01ddff2b5
rcalvs <rafaelcalvette@gmail.com>
2022-01-21 15:27:19 -0300

    Req 02-03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,21 @@
+def selection_sort(word):
+    array = list(word)
+    for index in range(len(array)):
+        minimum = index
+        for x in range(index + 1, len(array)):
+            if array[x] < array[minimum]:
+                minimum = x
+        array[minimum], array[index] = array[index], array[minimum]
+
+    return array
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    if selection_sort(first_string) == selection_sort(second_string):
+        return True
+    if selection_sort(first_string) != selection_sort(second_string):
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
+# https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
+
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if (low_index == high_index):
+        return True
+    if (word[low_index] != word[high_index]):
+        return False
+    if (low_index < high_index + 1):
+        return is_palindrome_recursive(word, low_index + 1,  high_index - 1)
+    return True
