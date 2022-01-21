36f402702b7b565ebf8b4dad68b584c9dec24699
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-01-21 14:02:12 -0300

    Requisito 02: palidromos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    splitted_word = list(word)
+
+    if len(word) == 0:
+        return False
+
+    if high_index < 0:
+        return True
+
+    if splitted_word[low_index] == splitted_word[high_index]:
+        if low_index <= (len(splitted_word) / 2):
+            return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+        return True
+    return False
