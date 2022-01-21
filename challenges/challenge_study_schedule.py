cf0aff324e19b89b2f58d9fdaae5122e6cd64209
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-01-20 23:06:32 -0300

    ch 1 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    if target_time is None:
+        return None
+    for student_period in permanence_period:
+        if student_period[0] is None or student_period[1] is None:
+            return None
+        if target_time >= student_period[0] and target_time <= student_period[1]:
+            count += 1
+    return count
+
+
+"""
+    cada tupla (horario_entrada, horario_saida)
+"""
