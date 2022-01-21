f16f64e1df08101d387fadb6c97fe3e62dbd53af
Carlos Sá <carlos25xrm@gmail.com>
2022-01-21 17:02:38 -0300

    fix: Adicionando comentário no requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
-    # Entendi parte do requisito 2 com ajuda do Rafael Medeiros. Grazie Mille!
+    # Entendi parte do requisito 2 com ajuda do Rafael Medeiros. Grazie Mille!*
     if (word is None or not word or word[low_index] != word[high_index]):
         return False
 
     if (low_index >= high_index):
         return True
 
+    # Usando recursividade abaixo*
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
     # ABAIXO NÃO FUNCIONOU
