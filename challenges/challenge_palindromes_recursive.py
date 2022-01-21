1e9b34fa4308ea470638a48c85a99ea3690ad14b
Hudson Carlos <hc88806@gmail.com>
2022-01-20 23:22:29 -0400

    challenge palidromes done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,16 @@
+def reversed_word(list):
+    if len(list) < 2:
+        return list
+    else:
+        return reversed_word(list[1:]) + [list[0]]
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    word = list(word)
+    reversed_list = reversed_word(word)
+
+    if " ".join(reversed_list) == " ".join(word):
+        return True
+    return False
