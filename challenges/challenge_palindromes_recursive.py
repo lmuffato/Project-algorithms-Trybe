be26b66f9322c2bfbb0775239c6b17ba62c27802
rafael <rafittu@outlook.com>
2022-01-21 18:17:01 -0300

    feat req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
