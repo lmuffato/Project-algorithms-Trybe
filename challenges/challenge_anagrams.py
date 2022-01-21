d7466d7318f550b500404f6cecd8dcd1200f272e
Bruno Mendes <mendessbruno@gmail.com>
2022-01-21 14:08:17 -0300

    resolvido desafio 3 parcialmente

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -3,22 +3,17 @@ def sortList(array):
 
     for i in range(length - 1):
         min_index = i
-
         for j in range(i + 1, length - 1):
-            if array[j] < array[min_index]:
+            if ord(array[j]) < ord(array[min_index]):
                 min_index = j
-
         array[i], array[min_index] = array[min_index], array[i]
+    return array
 
 
 def is_anagram(first_string, second_string):
-    replaced_one = first_string.replace(' ', '')
-    replaced_two = second_string.replace(' ', '')
-    sorted_one = sortList(list(replaced_one))
-    sorted_two = sortList(list(replaced_two))
-    print(sorted_one)
-    print(sorted_two)
-    # if sorted_one == sorted_two:
-    #     return True
-    # else:
-    #     return False
+    sorted_one = sortList(list(first_string))
+    sorted_two = sortList(list(second_string))
+    if sorted_one == sorted_two:
+        return True
+    else:
+        return False
