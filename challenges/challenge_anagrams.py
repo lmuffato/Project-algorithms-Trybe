016dfba6c039f90dc1616a83edc25c9f38cee42a
Carlos Sá <carlos25xrm@gmail.com>
2022-01-21 18:00:58 -0300

    feat: 3 - Anagramas (Algoritmo de ordenação) - TENTATIVA 1

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,18 @@
+# Tentativa #1 - Passando somente 2/4 dos testes
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (not first_string or not second_string):
+        return False
+
+    f_string = list(first_string)
+    s_string = list(second_string)
+    result = ''
+
+    for index in range(len(f_string)):
+        if (s_string[index] in f_string):
+            result = 'v'
+
+    if (result != 'v'):
+        return False
+
+    return True
