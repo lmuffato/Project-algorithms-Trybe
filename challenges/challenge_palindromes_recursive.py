f6f54af55e591366a029c32f864f21bc4495accb
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-01-20 23:25:27 -0300

    req 2 done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,21 @@
+"""
+https://www.askpython.com/python/array/reverse-an-array-in-python
+sobre a reversão da string
+"""
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if (high_index - low_index) in [0, 1]:
+        return True
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+"""
+low_index "menor index"
+high_index "maior index"
+"""
