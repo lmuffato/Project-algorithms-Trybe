a8b2d573884769344bfda0105b33c8a1e9ac37db
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-01-21 13:46:00 -0300

    Refactor: requisito 01 - study_schedule: complex

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,18 +1,17 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
-    
+
     for period in permanence_period:
         end = period[1]
         begin = period[0]
 
-        if(target_time == None or end == None or begin == None):
+        if target_time is None or end is None or begin is None:
             return None
-        
-        if(end == target_time):
+
+        if end == target_time or begin == target_time:
             counter += 1
-        elif(begin == target_time):
-            counter += 1
-        elif((end - begin) >= target_time and begin < target_time ):
+
+        if (end - begin) >= target_time and begin < target_time:
             counter += 1
 
     return counter
