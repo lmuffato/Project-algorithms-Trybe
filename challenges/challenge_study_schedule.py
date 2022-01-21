d65630349eb267c4cd61dbcc9366750c3a22b343
Carlos Sá <carlos25xrm@gmail.com>
2022-01-21 12:05:22 -0300

    fix: Corrigindo comentário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    # Consegui com ajuda do Leonardo Mallmann
+    # Consegui entender o erro do avaliador com ajuda do Leonardo Mallmann
     if (permanence_period[0][0] is None or permanence_period[0][1] is None):
         return None
     if (target_time is None):
