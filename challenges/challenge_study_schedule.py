ba01a528699ac73acf31f28eef2859827c466aa5
rcalvs <rafaelcalvette@gmail.com>
2022-01-21 13:55:29 -0300

    Req 01 OK

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    else:
+        counter = 0
+        for one_period in permanence_period:
+            if not all(one_period):
+                return None
+            if one_period[0] <= target_time <= one_period[1]:
+                counter += 1
+    return counter
