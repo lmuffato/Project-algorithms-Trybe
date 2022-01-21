5484758696a3e9e23686cee793dd9c3f312384c4
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2022-01-20 23:13:25 -0300

    .

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,10 @@ def study_schedule(permanence_period, target_time):
     count = 0
     if target_time is None:
         return None
-    for student_period in permanence_period:
-        if student_period[0] is None or student_period[1] is None:
+    for student_p in permanence_period:
+        if student_p[0] is None or student_p[1] is None:
             return None
-        if target_time >= student_period[0] and target_time <= student_period[1]:
+        if target_time >= student_p[0] and target_time <= student_p[1]:
             count += 1
     return count
 
