9d9844f844a8fb30091833047730c9dae24b5667
MaxRudim <max.hermsdorff@gmail.com>
2022-01-20 21:42:23 -0300

    Requisito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,24 @@
+# Usei o seletion sort do course da trybe:
+# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+
+def selection_sort(array):
+    for i in range(len(array)):
+        minimum = i
+
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        array[minimum], array[i] = array[i], array[minimum]
+
+    return array
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
-    #
+    if len(first_string) != len(second_string):
+        return False
+    sorted_word1 = selection_sort(list(first_string))
+    sorted_word2 = selection_sort(list(second_string))
+    if sorted_word1 != sorted_word2:
+        return False
+    return True
