b2c3ae94c41f1c7569d17029f5e420a3caf440e4
w-cypriano <profwellington17@gmail.com>
2022-01-21 15:17:10 -0300

    requisito1- funçao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count_students = 0
+    if not target_time or type(target_time) != int:
+        return None
+    else:
+        for student in permanence_period:
+            if type(student[0]) == int and type(student[1]) == int:
+                if student[0] <= target_time and student[1] >= target_time:
+                    count_students = count_students + 1
+            else:
+                return None
+        return count_students
