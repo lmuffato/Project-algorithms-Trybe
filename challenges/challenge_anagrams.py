bb600dbe6dc858a2bb65a98ada5317e3747c8cf1
rafael <rafittu@outlook.com>
2022-01-21 18:34:52 -0300

    feat partial of req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,5 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string or second_string == '':
+        return False
+    if len(first_string) != len(second_string):
+        return False
