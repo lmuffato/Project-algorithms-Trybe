2246f180480fabcd888354d4c9cff52050c5e806
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-01-21 14:21:34 -0300

    Requisito 03: anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
+from operator import indexOf
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    first_list = list(first_string)
+    second_list = list(second_string)
+
+    counter = 0
+
+    for letter in first_list:
+        for second_letter in second_list:
+            if letter == second_letter:
+                second_list.pop(indexOf(second_list, second_letter))
+                counter += 1
+
+    if counter == len(first_string):
+        return True
+
+    return False
