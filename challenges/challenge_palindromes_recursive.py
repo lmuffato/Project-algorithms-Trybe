c80dcad2df9731bb9c49c99453dae6124b938818
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2022-01-22 02:14:28 -0300

    feat(req02): implement is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if(word==''): return False
+
+    output = True and (word[low_index] == word[high_index])
+    low_index +=1
+    high_index -=1
+
+    if(low_index>high_index or low_index==high_index):
+      return output
+
+    return is_palindrome_recursive(word, low_index, high_index)
+
+# print(is_palindrome_recursive("ANA", 0, 2))
+# print(is_palindrome_recursive("SOCOS", 0, 4))
+# print(is_palindrome_recursive("REVIVER", 0, 6))
+# print(is_palindrome_recursive("COXINHA", 0, 6))
+# print(is_palindrome_recursive("AGUA", 0, 3))
