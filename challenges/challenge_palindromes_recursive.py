07d071158a16af5426576a8eb61d3d87775598b9
Luiz <luizfvaccarif@gmail.com>
2022-01-22 00:07:54 -0300

    feat: req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if low_index > len(word)/2:
+        return True
+    if len(word) == 0:
+        return False
+    if len(word) == 1:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+""" word = "REVIVER"
+teste = is_palindrome_recursive(word, 0, len(word) - 1)
+print(teste) """
