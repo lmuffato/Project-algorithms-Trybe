8d7b2b22b44f699a8f232911a1c02b5f8c73a3a8
Patrick Morais <ppternunes@gmail.com>
2022-01-22 17:20:46 -0300

    refactor

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,10 +4,8 @@ import math
 def is_palindrome_recursive(word, low_index, high_index):
     if not word:
         return False
-    if len(word) == 1 or low_index == high_index:
+    if len(word) == 1 or low_index == high_index or high_index < low_index:
         return True
     if word[low_index] != word[high_index]:
         return False
-    if low_index == math.floor(len(word) / 2):
-        return True
     return is_palindrome_recursive(word, low_index+1, high_index-1)
