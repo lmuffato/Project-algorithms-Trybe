e6e07640a7f97cff4f242beecc8bfd3cbd0dd35f
Luiz <luizfvaccarif@gmail.com>
2022-01-22 01:32:19 -0300

    fix: req 3.4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,19 +2,11 @@ def is_anagram(first_string, second_string):
     if first_string == '' or second_string == '':
         return False
     for f in first_string:
-        counter_f = count_f(f, first_string)
-        counter_s = count_f(f, second_string)
+        counter_f = first_string.count(f)
+        counter_s = second_string.count(f)
         if counter_f != counter_s:
             return False
     return True
-
-
-def count_f(f, string):
-    counter = 0
-    for i in string:
-        if f == i:
-            counter += 1
-    return counter
 
 
 """ first_string = 'perda'
