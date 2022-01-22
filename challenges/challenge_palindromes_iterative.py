d9f24f41c1eb5ccd89daf743ec9afa2a70542ad0
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2022-01-22 02:18:02 -0300

    feat(req5): implement is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,18 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if(len(word) == 0): return False
+    li = 0
+    hi = len(word)-1
+    output = True
+    while(li!=hi):
+      output = True and (word[li] == word[hi])
+      li +=1
+      hi -=1
+      if(li>hi):
+        break
+    return output
+
+# print(is_palindrome_recursive("ANA", 0, 2))
+# print(is_palindrome_recursive("SOCOS", 0, 4))
+# print(is_palindrome_recursive("REVIVER", 0, 6))
+# print(is_palindrome_recursive("COXINHA", 0, 6))
+# print(is_palindrome_recursive("AGUA", 0, 3))
