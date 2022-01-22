30d16d0527d640cfb1df1cd309b98ab8544c0c09
Bidfca <bruninhakjurugat11@gmail.com>
2022-01-22 05:50:26 -0300

    1-2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None:
+        return None
+    counter = 0
+    for permanence_tuple in permanence_period:
+        if None in permanence_tuple:
+            return None
+        if permanence_tuple[0] <= target_time <= permanence_tuple[1]:
+            counter += 1
+    return counter
