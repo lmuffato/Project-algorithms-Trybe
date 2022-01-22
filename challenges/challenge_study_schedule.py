6cd229dc2337004f01a3fc728bc1c6c51f60dd71
Michael <michaelpetterson06.com.br>
2022-01-22 17:10:51 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    total = 0
+    if target_time is None:
+        return None
+    for tupla in permanence_period:
+        if tupla[0] is None or tupla[1] is None:
+            return None
+        elif tupla[0] <= target_time <= tupla[1]:
+            total += 1
+    return total
