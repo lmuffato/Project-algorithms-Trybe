63f3b64436dcc9a4aaf40f2034060a03b5dda17d
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 10:04:12 -0300

    Feat Req2 - implementa a recursividade da função

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,3 +4,6 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if word[low_index] != word[high_index]:
         return False
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
