2fbd52d960fd7a4252d2d0f9c5d559679c9d5036
GuilhermeMD <guilherme.md@gmail.com>
2022-01-22 20:29:49 -0300

    req 01 - implementation

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+
+    students = 0
+    for time in permanence_period:
+        if (time[0] <= target_time <= time[1]):
+            students += 1
+    return students
+#requisito realizado com ajuda do colega Carlos Sá - Turma 10A
