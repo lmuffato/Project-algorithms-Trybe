837ab6e9df6a556e07d5e56ba019f6cbd0e83a13
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-01-22 12:10:29 -0300

    FEAT:Req02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if low_index >= high_index:
+        return word[low_index] == word[high_index]
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
