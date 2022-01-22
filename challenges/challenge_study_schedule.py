2fd7425ee834cb6771aef73f48cc1283876a0ad0
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 09:32:13 -0300

    Feat Req 01 - faz a validação se o começo e o fim sao menores e maiores que o target_time, caso sejam incrementa 1 ao best_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,6 +3,10 @@ def study_schedule(permanence_period, target_time):
     if target_time == "":
         return None
     else:
+        best_schedule = 0
         for start, end in permanence_period:
-            if start == "" or end == "":
+            if start is None or end is None:
                 return None
+            if (start <= target_time) and (end >= target_time):
+                best_schedule += 1
+    return best_schedule
