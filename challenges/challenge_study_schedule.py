8e7918808251f9bb673355d03aa325ac931fe87d
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 09:26:08 -0300

    Feat Req 01 - verifica se o começo ou fim é vazio

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,8 @@
 def study_schedule(permanence_period, target_time):
     # """ Faça o código aqui. """
-    if (target_time == ''):
+    if target_time == "":
         return None
+    else:
+        for start, end in permanence_period:
+            if start == "" or end == "":
+                return None
