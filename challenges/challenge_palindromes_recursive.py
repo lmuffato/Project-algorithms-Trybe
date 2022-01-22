b07e962a0e02b571398786d48f03dec0e38e62b1
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 09:55:21 -0300

    Feat Req 2 - Caso o index inicial da palavra seja diferente do final, ela não é um palindromo e retorna false

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -2,3 +2,5 @@ def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if word == "":
         return False
+    if word[low_index] != word[high_index]:
+        return False
