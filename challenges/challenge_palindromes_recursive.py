dcb20fe66230c8de679d761ed7142e59549728c1
Patrick Morais <ppternunes@gmail.com>
2022-01-22 16:25:01 -0300

    req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
+import math
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    if len(word) == 1 or low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index == math.floor(len(word) / 2):
+        return True
+    return is_palindrome_recursive(word, low_index+1, high_index-1)
