6eda9677ecbac48d7028fd773c416ef33edc25d7
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 10:30:07 -0300

    Iterative Palindrome - implementaçao da função, não tem avaliação no github

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -2,3 +2,12 @@ def is_palindrome_iterative(word):
     """ Faça o código aqui. """
     if word == "":
         return False
+    length = len(word) // 2
+    palindrome = True
+    for i in range(0, length):
+        left = word[i]
+        right = word[len(word)-i-1]
+        if left != right:
+            palindrome = False
+            break
+    return palindrome
