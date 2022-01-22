fce63f49fdb82839d8df35dcf7dc58591b0e0b36
Eduardo Seije <eduardoseije@gmail.com>
2022-01-22 11:00:21 -0300

    Feat req 03 - criando funções de validação de strings e anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,21 @@
+def anagram_check(first_string, second_string):
+    return False
+
+
+def strings_check(first_string, second_string):
+    if (
+        first_string == ""
+        or second_string == ""
+        or len(first_string) != len(second_string)
+    ):
+        return False
+    return True
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
-    if first_string == "" or second_string == "":
+    """Faça o código aqui."""
+    strings = strings_check(first_string, second_string)
+
+    if not strings:
         return False
-    if len(first_string) != len(second_string):
-        return False
-        
+    return True
