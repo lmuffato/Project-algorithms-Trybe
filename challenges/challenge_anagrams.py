4886907453d1dfd75790e013875eecb0a475fbac
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-22 19:05:15 -0300

    feat: req3.4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,25 @@
+def string_sort(string):
+    array = list(string)
+
+    for i in range(len(array)):
+        minimum = i
+
+        for j in range(i + 1, len(array)):
+            if array[j] < array[minimum]:
+                minimum = j
+        array[minimum], array[i] = array[i], array[minimum]
+
+    return "".join(array)
+
+
 def is_anagram(first_string, second_string):
     if not first_string or not second_string:
         return False
-    elif second_string[::-1] == first_string:
+
+    first_string_sorted = list(first_string)
+    second_string_sorted = list(second_string)
+
+    if first_string_sorted == second_string_sorted:
         return True
+
     return False
