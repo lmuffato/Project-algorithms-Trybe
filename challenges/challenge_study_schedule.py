518dba074cdb3bf2f251fb0f302ed2092e6fa781
Patrick Morais <ppternunes@gmail.com>
2022-01-22 16:03:08 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time == None:
+        return None
+    present_students = 0
+    for time_student_tuple in permanence_period:
+        if None in time_student_tuple:
+            return None
+        if time_student_tuple[0] <= target_time <= time_student_tuple[1]:
+            present_students += 1
+    return present_students
