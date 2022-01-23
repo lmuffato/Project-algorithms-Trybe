d3d45aa4d85d92324201caf2b16f62a697686bbc
igson <igson660@gmail.com>
2022-01-23 12:13:26 -0500

    quesito 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word:
+        if word[high_index] != word[low_index]:
+            return False
+
+        if -1 < (low_index - high_index):
+            return True
+        else:
+            return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
+
+    return False
