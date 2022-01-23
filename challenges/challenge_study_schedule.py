3671368a217fba9ffe427e971ff6eb155f3eb6f1
Elisa <elisafranca.carvalho@gmail.com>
2022-01-23 10:14:57 -0300

    feat: implements 1st requirement again

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,7 @@
 def study_schedule(permanence_period, target_time):
     students = []
     for start_time, finish_time in permanence_period:
-        # Fontes:
-        # https://www.w3schools.com/python/ref_func_isinstance.asp
+        # Fontes: https://www.w3schools.com/python/ref_func_isinstance.asp
         # https://pynative.com/python-isinstance-explained-with-examples/
         # https://www.youtube.com/watch?v=Yoi8gh9x5PU
         # Checando se as entradas de permanence period são válidas
