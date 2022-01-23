b04fbaa4ce53b11aca11f6a2bc6e3be72b72ad68
igson <igson660@gmail.com>
2022-01-23 12:00:47 -0500

    fix quesito 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    if not target_time: return None
+    if not target_time:
+        return None
 
     on_line = 0
     for horario in permanence_period:
-        if not horario[0] or not horario[1]: return None
-        if horario[0] <= target_time <= horario[1]: on_line += 1
+        if not horario[0] or not horario[1]:
+            return None
+        if horario[0] <= target_time <= horario[1]:
+            on_line += 1
 
     return on_line
