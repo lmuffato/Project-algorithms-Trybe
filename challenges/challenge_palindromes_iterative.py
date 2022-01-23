47112268a5ff28883e8fe91ddc3b31afcf5768b8
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2022-01-23 16:14:29 -0300

    refactor: utilizando operador de fatias

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,12 +1,6 @@
 def is_palindrome_iterative(word):
     if word:
-        reverse_index = len(word) - 1
-        reverse_word = []
-
-        while reverse_index >= 0:
-            reverse_word.append(word[reverse_index])
-            reverse_index -= 1
-
+        reverse_word = word[::-1]
         return True if ("".join(reverse_word) == word) else False
 
     return False
@@ -14,5 +8,6 @@ def is_palindrome_iterative(word):
 
 # revertendo listas
 # https://www.delftstack.com/pt/howto/python/python-reverse-a-list/
+
 
 # print(is_palindrome_iterative("ama"))
