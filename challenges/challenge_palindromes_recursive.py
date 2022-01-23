2afce7fadbbd2b01aebce9a5fc855c0841fbe512
Michael <michaelpetterson06.com.br>
2022-01-22 22:13:13 -0300

    req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if high_index == 0:
+        return True
+    elif high_index == -1:
+        return False
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
