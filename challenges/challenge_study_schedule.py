f54bdcaab2fd5a8b28d796903905c47680ed0240
Orlando Flores <orlando_flores@id.uff.br>
2022-01-23 16:08:56 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    students_in = 0
+    for in_time, out_time in permanence_period:
+        if in_time is None or out_time is None:
+            return None
+        if in_time <= target_time <= out_time:
+            students_in += 1
+    return students_in
