4fbe9794aa228b35bf773bc5ccebad6aeb103153
negres <nataliaknegres@gmail.com>
2022-01-23 17:40:24 -0300

    add req 3.2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,5 @@
 def is_anagram(first_string, second_string):
-    if len(first_string) == 0 or len(second_string) == 0:
+    if first_string or second_string == '':
         return False
     elif len(first_string) != len(second_string):
         return False
