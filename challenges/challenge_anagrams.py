edb5d7948171832d73dbe860020c4a53f723c10d
negres <nataliaknegres@gmail.com>
2022-01-23 17:20:42 -0300

    add reqs 1, 2, 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,5 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) == 0 or len(second_string) == 0:
+        return False
+    elif len(first_string) != len(second_string):
+        return False
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
+from audioop import reverse
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        count = 0
+        for item in permanence_period:
+            if target_time >= item[0] and target_time <= item[1]:
+                count += 1
+        return count
+    except TypeError:
+        return None
