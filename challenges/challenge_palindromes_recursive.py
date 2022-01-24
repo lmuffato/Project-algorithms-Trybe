48350e04b0e35706133d6e428fb4f33b06fc7fb3
w-cypriano <profwellington17@gmail.com>
2022-01-24 19:25:17 -0300

    fix: req1 e req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,27 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+        print("1")
+        """low_index = low_index + 1
+        high_index = high_index - 1"""
+
+    elif low_index < len(word) - 1:
+        low_index = low_index + 1
+        high_index = high_index - 1
+        return is_palindrome_recursive(word, low_index, high_index)
+
+    else:
+        return True
+        print("2")
+        """low_index = low_index + 1
+        high_index = high_index - 1"""
+
+
+low_index = 0
+word = "ana"
+resp = is_palindrome_recursive(word, low_index, len(word) - 1)
+print(resp)
