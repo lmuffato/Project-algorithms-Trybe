fc7e552899ad5500939b6cf33a15374233d34d28
Lucas André <lucas.cancado89@gmail.com>
2022-01-24 19:29:06 -0300

    req05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,5 +1,7 @@
 def find_duplicate(nums):
     for i in range(len(nums)):
+        if nums[i] < 0:
+            return False
         for index in range(len(nums)):
             if i != index and nums[i] == nums[index]:
                 return nums[i]
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    word_list = list(word)
+    for i in range(len(word)):
+        low = 0
+        high = len(word_list) - 1
+        while low - high not in (0, 1):
+            if word_list[low] != word_list[high]:
+                return False
+            low += 1
+            high -= 1
+    return True
