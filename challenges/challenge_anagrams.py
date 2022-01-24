c4ecc9af53970ccc49cf086c3a4f33d0b14f7d0f
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2022-01-24 20:06:24 -0300

    req 3 - sorting

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+def sort_string(str):
+    str_list = list(str)
+    swapped = True
+    num_of_iterations = 0
+
+    while swapped:
+        swapped = False
+
+        for i in range(len(str_list) - num_of_iterations - 1):
+            if str_list[i] > str_list[i + 1]:
+                str_list[i], str_list[i + 1] = str_list[i + 1], str_list[i]
+                swapped = True
+        num_of_iterations += 1
+
+    return ''.join(str_list)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if first_string == '' or second_string == '':
+        return False
+
+    first_string_sorted = sort_string(first_string)
+    second_string_sorted = sort_string(second_string)
+
+    return first_string_sorted == second_string_sorted
