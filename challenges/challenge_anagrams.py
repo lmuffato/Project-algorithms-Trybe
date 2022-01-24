b57e3a4c02905b6e0e9892062b791195fb224574
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 19:41:58 -0300

    feat: req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = list(first_string)
+    second = list(second_string)
+    
+    if len(first_string) != len(second_string):
+        return False
+    if first_string == '' or second_string == '':
+        return False
+    def sort(string):
+        ordem = []
+        new_array = ''
+        array = list(string)
+        while len(array) > 0:
+            low_letter = min(array)
+            array.remove(low_letter)
+            ordem.append(low_letter)
+        return new_array.join(ordem)
+    if sort(first_string) == sort(second_string):
+        return True
+    return False
