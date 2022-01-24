3ede74a3b3f9d3de991092e11beef2f148565990
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-24 18:56:46 -0300

    Implementa funçoes 'find_duplicate'

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    arr = nums
+
+    for i in range(len(arr)):
+        for prox in range(i + 1, len(arr)):
+            if type(arr[i]) == str or arr[i] < 0:
+                return False
+            if arr[prox] == arr[i]:
+                return arr[prox]
+
+    return False
