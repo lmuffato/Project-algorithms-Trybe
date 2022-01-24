a6af3a9c4419b1816ec03211ed2beea81ad24adb
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 19:49:19 -0300

    feat: req 3 refatorado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,12 +1,4 @@
-def is_anagram(first_string, second_string):
-    first = list(first_string)
-    second = list(second_string)
-    
-    if len(first_string) != len(second_string):
-        return False
-    if first_string == '' or second_string == '':
-        return False
-    def sort(string):
+def sort(string):
         ordem = []
         new_array = ''
         array = list(string)
@@ -15,6 +7,14 @@ def is_anagram(first_string, second_string):
             array.remove(low_letter)
             ordem.append(low_letter)
         return new_array.join(ordem)
-    if sort(first_string) == sort(second_string):
-        return True
+
+def is_anagram(first_string, second_string):
+    first = list(first_string)
+    second = list(second_string)
+    
+    if len(first_string) != len(second_string):
+        return False
+    if first_string == '' or second_string == '':
+        return False
+    return sort(first_string) == sort(second_string)
     return False
