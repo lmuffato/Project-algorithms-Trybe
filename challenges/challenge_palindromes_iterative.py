04ac60edab9bde23429564fb32638b81127c4362
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-24 18:59:02 -0300

    Implementa funçao 'is_palindrome_iterative'

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if len(word) == 1:
+        return True
+
+    low_index = 0
+    high_index = len(word) - 1
+    _break = len(word) // 2
+
+    for i in range(_break):
+        if word[low_index] == word[high_index]:
+            low_index += 1
+            high_index -= 1
+        if low_index == _break:
+            return True
+
+    return False
+
