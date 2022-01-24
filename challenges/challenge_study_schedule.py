c99aae03765bbb033a8c65cd019172c9fa73f0be
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 18:12:12 -0300

    Refazendo o req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,10 @@
 def study_schedule(permanence_period, target_time):
     n_estudantes = 0
+    if target_time == '':
+        return None
     for student in permanence_period:
+        if type(student[0]) != int or type(student[1]) != int:
+            return None
         if student[0] <= target_time and student[1] >= target_time:
             n_estudantes += 1
     return n_estudantes
