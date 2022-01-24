91ef684c4dfdc806308609e717f8d681fcd9642f
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2022-01-24 18:31:05 -0300

    req 1 - study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    student_count = 0
+    for time_period in permanence_period:
+        if time_period[0] is None or time_period[1] is None:
+            return None
+
+        if target_time >= time_period[0] and target_time <= time_period[1]:
+            student_count += 1
+
+    return student_count
