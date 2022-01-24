ab08676a54d0f366aad0edd10e5247c50b12cd6b
Fernando Castello Branco <fernandocastellob@gmail.com>
2022-01-24 15:04:29 -0300

    FEAT:Req03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,13 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    if (first_string[:2] == second_string[:2]
+            and first_string[-2:] == second_string[-2:]):
+        return True
+    inverse_first = (first_string[-3:-1][1], first_string[-3:-1][0])
+    inverse_second = (second_string[-3:-1][0], second_string[-3:-1][1])
+
+    if inverse_first != inverse_second:
+        return False
+
+    return True
