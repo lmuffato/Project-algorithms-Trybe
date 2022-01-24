c20c993fcc4536ccc4ec4909a8fbaef4ebdc38cd
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-24 18:50:20 -0300

    Implementa funçoes 'sort_str' e 'is_anagram'

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,24 @@
+def sort_str(_str):
+    str = list(_str)
+
+    for anterior in range(len(str)):
+        index = anterior
+
+        for proximo in range(anterior + 1, len(str)):
+            if str[proximo] < str[index]:
+                index = proximo
+
+        str[anterior], str[index] = str[index], str[anterior]
+
+    return "".join(str)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (
+        first_string == ""
+        or second_string == ""
+        or len(first_string) != len(second_string)
+    ):
+        return False
+
+    return sort_str(first_string) == sort_str(second_string)
