85f285c7aef002c32562502e1eb117999b6ef9bd
Luciano Amâncio <madruga665@hotmail.com>
2022-01-24 08:00:57 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    conunter = 0
+
+    try:
+        for index in permanence_period:
+            if target_time >= index[0] and target_time <= index[1]:
+                conunter += 1
+    except TypeError:
+        return None
+    return conunter
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 2))
