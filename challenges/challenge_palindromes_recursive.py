fa30a019bb0e256da217e98e226efd2bde2891ba
Lucas André <lucas.cancado89@gmail.com>
2022-01-24 16:19:48 -0300

    req02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    elif word[low_index] != word[high_index]:
+        return False
+    elif low_index == high_index or low_index - high_index == -1:
+        return True
+    else:
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,9 +3,6 @@ def study_schedule(permanence_period, target_time):
     for p in permanence_period:
         if type(p[0]) != int or type(p[1]) != int or type(target_time) != int:
             return None
-        index = p[0]
-        while index <= p[1]:
-            if index == target_time:
-                count += 1
-            index += 1
+        if p[0] <= target_time <= p[1]:
+            count += 1
     return count
