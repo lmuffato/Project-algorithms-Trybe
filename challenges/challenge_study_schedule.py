80a2e721b1a87706e2f597712b543b6676a94985
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 17:20:56 -0300

    feat: req 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,6 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    n_estudantes = 0
+    for student in permanence_period:
+       if student[0] <= target_time and student[1] >= target_time:
+           n_estudantes += 1
+    return n_estudantes
