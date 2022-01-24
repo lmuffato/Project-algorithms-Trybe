b8b76b1dc46af45b51d31c31c56c29a2816b2cae
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 16:25:04 -0300

    feat: req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
+import math
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if math.ceil(len(word)/2) == high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
