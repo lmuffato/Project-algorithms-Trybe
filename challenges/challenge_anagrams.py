c76540ae1b38f2d1f7244a0825fe93497dc8c433
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 20:06:01 -0300

    feat: req 3 refatorado e corrigido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,15 @@
 def sort(string):
-        ordem = []
-        new_array = ''
-        array = list(string)
-        while len(array) > 0:
-            low_letter = min(array)
-            array.remove(low_letter)
-            ordem.append(low_letter)
-        return new_array.join(ordem)
+    ordem = []
+    new_array = ''
+    array = list(string)
+    while len(array) > 0:
+        low_letter = min(array)
+        array.remove(low_letter)
+        ordem.append(low_letter)
+    return new_array.join(ordem)
+
 
 def is_anagram(first_string, second_string):
-    first = list(first_string)
-    second = list(second_string)
-    
     if len(first_string) != len(second_string):
         return False
     if first_string == '' or second_string == '':
