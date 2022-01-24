153be4b9014001d5c60b8904d383d890753e44e5
biazidioti <bzferreira.bio@gmail.com>
2022-01-24 17:25:09 -0300

    feat: req 1 fix identation error

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,6 +1,6 @@
 def study_schedule(permanence_period, target_time):
     n_estudantes = 0
     for student in permanence_period:
-       if student[0] <= target_time and student[1] >= target_time:
-           n_estudantes += 1
+        if student[0] <= target_time and student[1] >= target_time:
+            n_estudantes += 1
     return n_estudantes
