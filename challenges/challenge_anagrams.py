d4b660df148c4716badc2b8ba4a9d099071b7944
Lucas André <lucas.cancado89@gmail.com>
2022-01-24 18:26:13 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,20 @@
+def reorder(word):
+    listed = list(word)
+    swaped = True
+    num_of_iterations = 0
+    while(swaped):
+        swaped = False
+        for i in range(len(listed) - num_of_iterations - 1):
+            if listed[i] > listed[i + 1]:
+                listed[i], listed[i + 1] = listed[i + 1], listed[i]
+                swaped = True
+        num_of_iterations += 1
+    return "".join(listed)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # if len(first_string) != len(second_string):
+    #     return False
+    if (reorder(first_string) == reorder(second_string)):
+        return True
+    return False
