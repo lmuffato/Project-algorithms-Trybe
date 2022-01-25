65f4b896fdd199f53ed084b3045d12ae00f20745
Maria Baeta <m.martinsdg@gmail.com>
2022-01-24 21:57:55 -0300

    feat: :sparkles: Requisito 2

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,16 @@
+# https://acervolima.com/programa-python-para-verificar-se-uma-string-e-palindromo-ou-nao/
+
+
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    length = len(word)
+
+    if length < 2:
+        return True
+
+    elif word[0] == word[length - 1]:
+
+        return is_palindrome_iterative(word[1 : length - 1])
+
+    else:
+        return False
