6f9ebfb4868fba7746d47f4d3fb2f6e31b536d3e
Maria Baeta <m.martinsdg@gmail.com>
2022-01-24 22:47:58 -0300

    feat: :sparkles: Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,22 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+
+    umalista1 = list(first_string)
+    umalista2 = list(second_string)
+
+    umalista1.sort()
+    umalista2.sort()
+
+    pos = 0
+    iguais = True
+
+    while pos < len(first_string) and iguais:
+        if umalista1[pos] == umalista2[pos]:
+            pos = pos + 1
+        else:
+            iguais = False
+
+    return iguais
+
+
+is_anagram("abcde", "edcba")
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -11,4 +11,4 @@ def is_palindrome_recursive(word, low_index, high_index):
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
 
-# print(is_palindrome_recursive("ana"))
+# is_palindrome_recursive("ana")
