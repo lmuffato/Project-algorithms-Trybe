cc566c5f4f5f6c698eb1821e3aae6991f1ffcfcd
Nathalia Zebral <natzebral@gmail.com>
2022-01-25 19:31:06 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (not first_string or not second_string):
+        return False
+    word_two = list(second_string)
+    word_one = list(first_string)
+    anagram = ''
+    for index in range(len(word_one)):
+        if (word_two[index] in word_one):
+            anagram = 'true'
+    if (anagram != 'true'):
+        return False
+    return True
