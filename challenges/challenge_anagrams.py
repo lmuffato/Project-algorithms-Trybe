f301138983708db7c23620c28d724f0c191fb246
Debora <deborinhapassos@gmail.com>
2022-01-25 12:35:58 -0300

    Req 3 - parcial - correção indentação

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -5,8 +5,8 @@ def is_anagram(first_string, second_string):
     string_1 = list(first_string)
     res = ''
     for index in range(len(string_1)):
-     if (string_2[index] in string_1):
-        res = 'true'
+        if (string_2[index] in string_1):
+            res = 'true'
     if (res != 'true'):
         return False
     return True
