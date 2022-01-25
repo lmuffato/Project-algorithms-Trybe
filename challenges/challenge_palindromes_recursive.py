306f2ccb869f38e32a28efa6fc22b27ce1bfec8c
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-25 19:54:35 -0300

    Feat: palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if len(word) == 0 or word is None:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index or len(word) == 1:
+        return True
+    is_palindrome_recursive(word, low_index + 1, high_index - 1)
