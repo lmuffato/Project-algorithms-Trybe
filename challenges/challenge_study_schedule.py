7d3fe54154c8659ba5efd53f5be81e86e6390285
Debora <deborinhapassos@gmail.com>
2022-01-25 12:26:17 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+    STUDIES = 0
+    for DATA in permanence_period:
+        if (DATA[0] <= target_time <= DATA[1]):
+            STUDIES += 1
+    return STUDIES
