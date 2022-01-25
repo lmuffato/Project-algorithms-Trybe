5945b4474b2994fd032272a5b554830cf28174ab
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-01-24 20:51:24 -0400

    feat: :sparkles: Requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    index = 0
+    try:
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                index += 1
+        return index
+    except TypeError:
+        return None
