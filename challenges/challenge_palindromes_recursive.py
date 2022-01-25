dbb810a34df40de339c3f26b073c4999163fd06d
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-25 11:31:13 -0300

    Feat 2: created a fuction to use recursive form to know if a word is palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,17 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+
+    if len(word) == 0:
+        return False
+
+    if low_index > (len(word) // 2):
+        return True
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
+
+
+# Utilizei o operador // para trazer um resultado aredondado, pois quando eu
+# estava usando apenas o / ele falhava no teste. Utilizei de
+# ref o site: https://www.w3schools.com/python/python_operators.asp
