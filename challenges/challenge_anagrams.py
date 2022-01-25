2577e749769f3a91a69599ef9d4faf749c1b1ef2
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-25 12:02:27 -0300

    Feat 3: created a function to sort a word

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
+def sort_strings(string):
+    string_list = []
+    string_list[:0] = string
+
+    for index1 in range(len(string_list) - 1):
+        for index2 in range(index1 + 1, len(string_list)):
+            if string_list[index1] > string_list[index2]:
+                temp_list = string_list[index1]
+                string_list[index1] = string_list[index2]
+                string_list[index2] = temp_list
+
+    return print(string_list)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+
+    return True
