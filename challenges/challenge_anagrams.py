68d0636709bf674a0b49f24cb6d2e7912de9db1c
Debora <deborinhapassos@gmail.com>
2022-01-25 12:34:05 -0300

    Req 3 - parcial

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui"""
+    if (not first_string or not second_string):
+        return False
+    string_2 = list(second_string)
+    string_1 = list(first_string)
+    res = ''
+    for index in range(len(string_1)):
+     if (string_2[index] in string_1):
+        res = 'true'
+    if (res != 'true'):
+        return False
+    return True
