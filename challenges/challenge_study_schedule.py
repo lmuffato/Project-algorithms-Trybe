ae74bbb943514bd26c1894a74207f08014bab74a
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-25 10:55:59 -0300

    Feat 1: create a function to return the number of students in a certain hour

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """ Inicio do projeto """
+    number_of_students = 0
+
+    if target_time is None:
+        return None
+
+    for student_time in permanence_period:
+
+        index = 0
+
+        if student_time[index] is None or student_time[index + 1] is None:
+            return None
+
+        if student_time[index] <= target_time <= student_time[index + 1]:
+            number_of_students += 1
+
+    return number_of_students
