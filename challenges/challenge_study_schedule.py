5c7c2233beb4922833ae390d4ffadff974cc5941
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-25 06:54:20 -0300

    Feat: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    sum = 0
+    for (beginning_time, end_time) in permanence_period:
+        if beginning_time is None or end_time is None or target_time is None:
+            return None
+        else:
+            
+            if beginning_time <= target_time <= end_time:
+                sum += 1
+    return sum
