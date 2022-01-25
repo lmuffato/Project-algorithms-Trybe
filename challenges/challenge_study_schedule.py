cb407f54c12f4a38007aa170039f72ae22e3d4b0
Alexandre Moraes <alexandre.moraes@vilaapps.com>
2022-01-25 19:38:59 -0300

    fix req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,9 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-    else:
+    try:
         counter = 0
         for (start, end) in permanence_period:
             if start <= target_time and end >= target_time:
                 counter += 1
-    return counter
+        return counter
+    except TypeError:
+        return None
