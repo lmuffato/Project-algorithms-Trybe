bb943ad9187f26c0787c4638cb5339eeae9f3b02
Nathalia Zebral <natzebral@gmail.com>
2022-01-25 19:26:44 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+    people = 0
+    for schedule_period in permanence_period:
+        if (schedule_period[0] <= target_time <= schedule_period[1]):
+            people += 1
+    return people
