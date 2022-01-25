3cbb97130a9decae6edd17534a3f698322c581a8
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-01-24 21:22:55 -0400

    3.1/3.2

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (
+        not(first_string and second_string)
+        or (len(first_string) != len(second_string))
+    ):
+        return False
+    return True
