86dbc94cd1aa9807fc312c5bccfc80306ac1e8fc
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-25 12:50:51 -0300

    Feat 3: created a function to verify if two words are an anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -9,11 +9,23 @@ def sort_strings(string):
                 string_list[index1] = string_list[index2]
                 string_list[index2] = temp_list
 
-    return print(string_list)
+    return string_list
 
 
 def is_anagram(first_string, second_string):
     if len(first_string) != len(second_string):
         return False
 
-    return True
+    sort_one = sort_strings(first_string)
+    sort_two = sort_strings(second_string)
+
+    if ''.join(map(str, sort_one)) == ''.join(map(str, sort_two)):
+        return True
+    else:
+        return False
+
+# Utilização da função map através do link:
+# https://www.simplilearn.com/tutorials/python-tutorial/list-to-string-in-python
+
+# Entendimento do Bubble Sort através do link:
+# https://stackoverflow.com/questions/13101468/python-how-to-sort-the-alphabet-in-a-list-without-sorted-functions
