da2e38485379914b465276c44b8c1a7febf6b351
w-cypriano <profwellington17@gmail.com>
2022-01-25 08:58:03 -0300

    requisito4 incompleto

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,5 +1,21 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    if len(first_string) != len(second_string):
+        return False
+    for first in first_string:
+        if first not in second_string:
+            return False
+        else:
+            second_string = second_string.replace(first, "")
+
+    return True
 
 
 # vqv
+first_string = "pedrra"
+second_string = "pedraaaaaaaa"
+
+teste = is_anagram(first_string, second_string)
+print(teste)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,9 +5,6 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     if word[low_index] != word[high_index]:
         return False
-        print("1")
-        """low_index = low_index + 1
-        high_index = high_index - 1"""
 
     elif low_index < len(word) - 1:
         low_index = low_index + 1
@@ -16,9 +13,6 @@ def is_palindrome_recursive(word, low_index, high_index):
 
     else:
         return True
-        print("2")
-        """low_index = low_index + 1
-        high_index = high_index - 1"""
 
 
 low_index = 0
