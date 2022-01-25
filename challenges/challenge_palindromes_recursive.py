599106e3ccecc11c8f8379c2cd1f712a0d815e71
Elisa <elisafranca.carvalho@gmail.com>
2022-01-24 21:42:21 -0300

    feat: implements 2nd requirement

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    # Verificando se a palavra é uma string vazia e se não é um palíndromo
+    if len(word) == 0 or word[low_index] != word[high_index]:
+        return False
+
+    elif (low_index >= high_index):
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# print(is_palindrome_recursive("ANA", 0, 2))
+# print(is_palindrome_recursive("COXINHA", 0, 6))
