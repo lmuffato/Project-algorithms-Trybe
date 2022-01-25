815a500b519de191c306dd9a0a4abc3585c51268
Edrei <edreimateus@gmail.com>
2022-01-25 20:58:32 -0300

    feat 1 requiriment

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    counter = 0
+    if target_time is None:
+        return None
+
+    for period in permanence_period:
+        if period[0] is None or period[1] is None:
+            return None
+        if target_time >= period[0] and target_time <= period[1]:
+            counter += 1
+
+    return counter
