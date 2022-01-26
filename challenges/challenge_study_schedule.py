89cae0072ad8f48544056bd1ba99cc001b1112da
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-26 08:08:22 -0300

    requirement:01, test

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    try:
+        for i in permanence_period:
+            if target_time >= i[0] and target_time <= i[1]:
+                count += 1
+    except TypeError:
+        return None
+    return count
