757f63618a8c63ee7a01e36d057eb4f3c4dd6adb
FIXsartoreto <fixsartoreto@gmail.com>
2022-01-26 09:42:48 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    count = 0
+    for student in permanence_period:
+        if not target_time or not student[0] or not student[1]:
+            return None
+
+        if target_time >= student[0] and target_time <= student[1]:
+            count += 1
+    return count
