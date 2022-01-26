ca1814b48cc3a04584191e9661214798f05ce0b2
luanlsr <luanfswd@gmail.com>
2022-01-25 23:14:34 -0300

    requisito 1 finalizado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui . """
+    # requisito feito com ajuda do Guilherme Dornelles
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+
+    students = 0
+    for study_time in permanence_period:
+        if (study_time[0] <= target_time <= study_time[1]):
+            students += 1
+    return students
