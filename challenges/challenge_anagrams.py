f25730935f267c02268bdc06613de7efa462ab93
Edrei <edreimateus@gmail.com>
2022-01-26 20:26:49 -0300

    fix sintax

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -2,6 +2,6 @@ def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
     if not first_string or not second_string:
         return False
-
     if len(first_string) != len(second_string):
         return False
+    return True
