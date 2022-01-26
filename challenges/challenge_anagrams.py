c76fc9da5198c79390c5c719fcf2cd79156858a4
Willian Alves <willian.alvesb65@gmail.com>
2022-01-26 20:13:00 -0300

    Feat: req 3 implementa  Algoritmo de ordenação

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    sumString = sum(map(ord, second_string))
+    sumWword = sum(map(ord, first_string))
+    result = False
+
+    if(sumWword == sumString):
+        for letter in second_string:
+            if(len(first_string) == len(second_string)):
+                if(first_string.__contains__(letter)):
+                    result = True
+                else:
+                    result = False
+            else:
+                result = False
+
+    return result
