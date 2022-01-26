976f6bd07709bf9969319284158a1c133210a75e
Edrei <edreimateus@gmail.com>
2022-01-26 20:22:07 -0300

    update requiriments

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+
+    if len(first_string) != len(second_string):
+        return False
