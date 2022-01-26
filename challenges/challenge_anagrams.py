f83e40065d6776b1c45501d4b452180843920019
Tiago Santos <tiago881@gmail.com>
2022-01-26 19:42:28 -0300

    refactor: ✨ requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,26 @@
+def found_string(string):
+    list_str = list(string)
+
+    for i in range(len(list_str)):
+        position = i
+
+        for j in range(i + 1, len(list_str)):
+            if list_str[j] < list_str[position]:
+                position = j
+        list_str[position], list_str[i] = list_str[i], list_str[position]
+
+    return "".join(list_str)
+
+
 def is_anagram(first_string, second_string):
     if not first_string or not second_string:
         return False
-    elif second_string[::-1] == first_string:
+
+    first_str_found = list(first_string)
+    second_str_found = list(second_string)
+
+    if first_str_found == second_str_found:
         return True
+
     return False
+# Solução realizado com auxilio do André Barroso T10A
