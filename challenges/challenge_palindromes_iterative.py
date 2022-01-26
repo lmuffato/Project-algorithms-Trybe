be487af2774abc31bb19d6e4fe6f787e8ed9f257
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-26 09:06:05 -0300

    feat/requirement:02

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    size = len(word)
+    if size == 0:
+        return False
+    for i in range(0, size // 2):
+        if word[i] != word[size - i - 1]:
+            return False
+    return True
+
+# referencia
+# https://pt.stackoverflow.com/questions/120421/como-identificar-se-uma-string-%C3%A9-um-pal%C3%ADndromo
