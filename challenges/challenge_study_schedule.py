518dede65d6891195fcbcbd6441860bf46b06a14
Willian Alves <willian.alvesb65@gmail.com>
2022-01-25 22:18:30 -0300

    Feat: req 1 implementa (Algoritmo de busca) Número de estudantes estudando no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    period_sum = 0
+    
+    if(type(target_time) != int):
+        return None
+    for student in permanence_period:
+        if student[0] and student[1]:
+            if(target_time >= student[0] and target_time <= student[1]):
+                period_sum += 1
+        else:
+            return None
+
+    return period_sum
+    
