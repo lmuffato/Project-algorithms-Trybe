c924211f6136c4f1ada29c20a8be6cb0c03a38e9
igson <igson660@gmail.com>
2022-01-25 23:21:40 -0500

    fix quesito 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,6 +1,6 @@
 def is_anagram(first_string, second_string):
     if not first_string or not second_string:
-      return False
+        return False
 
     if len(first_string) != len(second_string):
         return False
