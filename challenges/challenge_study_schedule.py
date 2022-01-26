966ddfd5da210a7f3c4df4e3b34d9d0c2096d673
Mathaus Annibal <mathausannibal@gmail.com>
2022-01-26 09:12:07 -0300

    adiciona requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if not target_time:
+        return None
+    sum_studentes = 0
+    for student in permanence_period:
+        # usei o not pois o type o teste não passava!!!
+        if not student[0] or not student[1]:
+            return None
+        if student[0] <= target_time and student[1] >= target_time:
+            sum_studentes += 1
+
+    return sum_studentes
