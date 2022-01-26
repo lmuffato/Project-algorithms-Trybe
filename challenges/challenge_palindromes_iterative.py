815133100ecf5552d6144578d3f378901aed94e1
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-26 17:09:13 -0300

    logic implementation

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,20 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    try:
+        palavra = len(word)
+        x = 0
+        low_index = 0
+        high_index = -1
+        while x < palavra:
+            if word[low_index] == word[high_index]:
+                low_index += 1
+                high_index -= 1
+                x = x + 1
+            else:
+                return False
+        return True
+    except KeyError:
+        raise False
+    
+
+# print(is_palindrome_recursive("racecar", 0, 6))
+
