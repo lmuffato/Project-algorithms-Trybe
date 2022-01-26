4ef700163d19dcd220ae1e7cf7452cdb8eeb1a0a
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-26 16:07:18 -0300

    first task - done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        students = 0
+        for student_in, student_out in permanence_period:
+            if student_in <= target_time <= student_out:
+                students += 1
+        return students
+    except TypeError:
+        raise None
