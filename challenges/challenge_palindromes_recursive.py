200b9cfa7acd255f5986b825c9fd65a9fc46fd60
Tiago Santos <tiago881@gmail.com>
2022-01-26 17:17:40 -0300

    feat: ✨ requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word == "" or word[low_index] != word[high_index]:
+        return False
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
