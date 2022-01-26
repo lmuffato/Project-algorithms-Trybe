70984cab1e5f20330b7a2c3755b9534727199867
FIXsartoreto <fixsartoreto@gmail.com>
2022-01-26 09:58:54 -0300

    Requisito 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,4 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    return True
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,14 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if not nums:
+        return False
+
+    array = nums.copy()
+    array.sort()
+
+    for i in range(1, len(array)):
+        first = array[i]
+        second = array[i - 1]
+        if first == second and min(first, second) > 0:
+            return first
+
+    return False
