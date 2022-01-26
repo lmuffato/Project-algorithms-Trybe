65ce6b6752cd8c8aed793da9e5bb23a8ee2796c4
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-01-26 19:16:45 -0300

    requeriment 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    student_counter = 0
+    for time_period in permanence_period:
+        try:
+            if time_period[0] <= target_time <= time_period[1]:
+                student_counter += 1
+        except TypeError:
+            return None
+    return student_counter
