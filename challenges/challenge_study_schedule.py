f979a285cca8abed12ed6fc6527fd16a080585d1
Tiago Santos <tiago881@gmail.com>
2022-01-26 14:22:21 -0300

    feat: ✨ requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    index = 0
+    for indice in permanence_period:
+        if indice[0] is None or indice[1] is None:
+            return None
+        elif indice[0] <= target_time <= indice[1]:
+            index += 1
+
+    return index
