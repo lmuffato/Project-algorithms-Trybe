bd1c5c36c38d95e845bdd811857a8c289f800f55
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-26 18:02:55 -0300

    anagram fct done

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+def alfabeth_list(string):
+    new_list=[]
+    chars = list(string)
+    while chars:
+        lower_char = chars[0]
+        for char in chars:
+            if char < lower_char:
+                lower_char = char
+        new_list.append(lower_char)
+        chars.remove(lower_char)
+    return new_list
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    return alfabeth_list(first_string) == alfabeth_list(second_string)
