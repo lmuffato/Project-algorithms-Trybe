074515dfc249c69f61b606eb3d3a437758f7be12
Willian Alves <willian.alvesb65@gmail.com>
2022-01-25 23:03:35 -0300

    Feat: req 2 implementa Palíndromos (Recursividade)

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,15 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    numberOne = 1
+
+    if(word.isalpha()):
+        if(high_index < low_index):
+            return True
+        else:
+            if(word[low_index] == word[high_index]):
+                return is_palindrome_recursive(
+                    word, low_index + numberOne, high_index - numberOne
+                )
+            else:
+                return False
+                
+    return False
