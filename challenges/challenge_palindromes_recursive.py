7aa00aed3a363a601ab7539230daf3defecbbcc7
William <w.willers@outlook.com>
2022-01-25 23:32:51 -0400

    Terceiro desafio

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if word == '':
         return False
-    
+ 
     floor_number = len(word) // 2
     if floor_number == low_index:
         return True
