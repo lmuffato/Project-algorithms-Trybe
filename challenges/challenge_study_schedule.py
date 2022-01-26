869204235cc903eb351d7c3286736424ac72dac3
williamDieter88 <williamzdieter@gmail.com>
2022-01-26 18:14:23 -0300

    Add reqs 1 and 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+
+    students = 0
+    for study_time in permanence_period:
+        if (study_time[0] <= target_time <= study_time[1]):
+            students += 1
+    return students
