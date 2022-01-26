cc8fc06b76558a5a321b59da64d14e5bfde04f73
William <w.willers@outlook.com>
2022-01-25 21:49:03 -0400

    Palindromos

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    
+    floor_number = len(word) // 2
+    if floor_number == low_index:
+        return True
+    elif word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
