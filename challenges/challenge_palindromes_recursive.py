f91797990166d3f832b9e2d148733c5bf8a43b07
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-26 16:56:17 -0300

    task2 complete

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if (not word or word[0] != word[-1]):
+        return False
+    if len(word) == 2 and word[0] == word[1]:
+        return True
+    if len(word) == 1:
+        return True
+    return is_palindrome_recursive(word[1:-1], low_index, high_index)
