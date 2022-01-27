4b039f5b370f2fd027ee7ad277a808e34ce6f237
vinicios <soicinivt@gmail.com>
2022-01-27 03:28:46 -0300

    feat(req 01): functions occurrences and study_schedule in challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,17 @@
+def occurrences(permanence_period, target_time):
+    occurrence = 0
+    for period in permanence_period:
+        if period[0] <= target_time <= period[1]:
+            occurrence += 1
+
+    return occurrence
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    try:
+        if target_time >= 0:
+            return occurrences(permanence_period, target_time)
+
+        return None
+    except TypeError:
+        return None
