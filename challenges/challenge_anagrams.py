f31f69a4869644a85a6d5f990f9f87b0f2d5771b
Elisa <elisafranca.carvalho@gmail.com>
2022-01-26 21:04:37 -0300

    feat: implements 3rd requirement

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,23 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # Fontes: https://www.programiz.com/python-programming/methods/built-in/ord
+    # https://www.digitalocean.com/community/tutorials/how-to-use-the-python-map-function-pt
+    # http://pythonclub.com.br/algoritmos-ordenacao.html
+    first_string_sum = sum(map(ord, first_string))
+    second_string_sum = sum(map(ord, second_string))
+
+    if(first_string_sum != second_string_sum):
+        return False
+
+    if(len(first_string) != len(second_string)):
+        return False
+
+    for letter in second_string:
+        if(first_string.__contains__(letter)):
+            result = True
+        else:
+            result = False
+    return result
+
+
+# print(is_anagram('amor', 'roma'))
+# print(is_anagram('amor', 'odio'))
