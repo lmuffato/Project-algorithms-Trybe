afdd32b08b9458629dfd4ae079074f7a68496a13
VRodriguera <V.rodriguesjk@gmail.com>
2022-01-26 21:03:59 -0300

    feat: :sparkles: Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if (permanence_period[0][0] is None or permanence_period[0][1] is None):
+        return None
+    if (target_time is None):
+        return None
+    studants = 0
+    for time in permanence_period:
+        if (time[0] <= target_time <= time[1]):
+            studants += 1
+    return studants
