099e3d002da42e9d228ead552a6d9235848503e1
Rmgreco <rafaelmgreco@hotmail.com>
2022-01-27 10:30:37 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
+def order_string(string):
+    ordered_string = ""
+    while len(ordered_string) < len(string):
+        lower = min(string)
+        ordered_string += lower
+        string = string.replace(lower, "", 1)
+    return ordered_string
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui """
+    if first_string == "" or second_string == "":
+        return False
+    return order_string(first_string) == order_string(second_string)
