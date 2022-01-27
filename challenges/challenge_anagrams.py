464fcd4700caf4be2aa669e9bff3dbb5a1ba6806
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-26 22:05:27 -0300

    test: requirement 03, errado

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    w1 = list(first_string)
+    w2 = list(second_string)
+    if (first_string == "" or second_string == ""):
+        return False
+    if (w2 == w1):
+        return True
+    if (w2 != w1):
+        return False
+    return True
