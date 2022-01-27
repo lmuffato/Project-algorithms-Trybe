069a66739eee839eb1dcd8d36dd9449b155033a6
Rmgreco <rafaelmgreco@hotmail.com>
2022-01-27 09:59:31 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
+    for (enter, leave) in permanence_period:
+        try:
+            if enter <= target_time <= leave:
+                count += 1
+        except TypeError:
+            return None
+    return count
