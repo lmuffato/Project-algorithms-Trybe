8041dbaf8bd94b2efbf24c2473a815883ba5f22c
jvitorof <j.vitoroflores@gmail.com>
2022-01-27 01:11:41 -0300

    Rated R

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if len(word) < 1:  # comprimento zero não é palindromo
+        return False
+    if low_index >= len(word) // 2:  # percorreu metade?
+        return True
+    if word[low_index] != word[high_index]:  # compara indices equidistantes
+        return False
+    # testa próximo indice
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
