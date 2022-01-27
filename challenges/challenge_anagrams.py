31ee50fba4b9f406677210fd633780ac2d17e060
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-01-27 05:04:55 -0400

    requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,25 @@
+def get_lowest(arr):
+    lowest_num = arr[0]
+    for i in range(len(arr)):
+        if lowest_num > arr[i]:
+            lowest_num = arr[i]
+    return lowest_num
+
+
+def sort_list(arr):
+    arr_it = list(arr)
+    new_arr = []
+    while len(new_arr) != len(arr):
+        lowest = get_lowest(arr_it)
+        new_arr.append(lowest)
+        arr_it.remove(lowest)
+    return "".join(new_arr)
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    first = sort_list(first_string)
+    second = sort_list(second_string)
+    if first == second:
+        return True
+    return False
