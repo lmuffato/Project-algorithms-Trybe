c406e689e079640b55f3e2260947d5ecbed23c8e
jvitorof <j.vitoroflores@gmail.com>
2022-01-27 00:41:47 -0300

    :rage5:

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    students = 0
+    if target_time is None:
+        return None
+    else:
+        for n in permanence_period:
+            entry = n[0]
+            exit = n[1]
+            if entry <= target_time and exit >= target_time:
+                students += 1
+        return students
+
