3ce89083e693d5c3f2d4044681bfd779fdff1f8b
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-27 08:25:51 -0300

    Fix:flake  anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,7 @@
-## https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
+# https://app.betrybe.com/course/computer-science/algoritmos/algoritmos
+# -de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/
+# 60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999
+# -673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
 def insertion_sort(array):
     # itera sobre cada valor do array
     for i in range(len(array)):
@@ -27,4 +30,3 @@ def is_anagram(first_string, second_string):
                 return False
         return True
     return False
-
