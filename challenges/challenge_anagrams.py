a110f6552aa66dea60602276cdf3c0364488cda7
VRodriguera <V.rodriguesjk@gmail.com>
2022-01-26 22:17:35 -0300

    feat: :sparkles: Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,12 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if (not first_string or not second_string):
+        return False
+    S_STRING_LIST = list(second_string)
+    F_STRING_LIST = list(first_string)
+    RESULT = ''
+    for index in range(len(F_STRING_LIST)):
+        if (S_STRING_LIST[index] in F_STRING_LIST):
+            RESULT = 'V'
+    if (RESULT != 'V'):
+        return False
+    return True
