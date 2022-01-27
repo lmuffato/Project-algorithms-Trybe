a6ec5f1f4e0012829c8497f780788e0995e7e0aa
Mateus <mtauws@gmail.com>
2022-01-27 13:07:23 -0300

    chore: req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    counter = 0
+    for period in permanence_period:
+        if not period[0] or not period[1]:
+            return None
+        if target_time >= period[0] and target_time <= period[1]:
+            counter += 1
+
+    return counter
