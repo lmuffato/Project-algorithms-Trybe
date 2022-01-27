3af5b1765308ac06556abc1f1fbc86e240d30bd5
jvitorof <j.vitoroflores@gmail.com>
2022-01-27 01:40:57 -0300

    RAGE AGAINST THE MACHINE

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False  # sem palavras sai
+    for index, letter in enumerate(word):
+        if word[index] != word[::-1][index]:  # compara o invertido e sai
+            return False
+
+    return True  # tudo certo só vai
