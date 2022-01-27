0bb7f6dcf325e1c75615891dcd1e380ca0a09677
Rmgreco <rafaelmgreco@hotmail.com>
2022-01-27 10:09:30 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,10 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    if low_index >= high_index:
+        if word[low_index] == word[high_index]:
+            return True
+        else:
+            return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
