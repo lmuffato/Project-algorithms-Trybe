517d60be3c0908404058ca8222480d0927dd24b4
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-27 21:29:08 -0400

    add: validations on study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,23 @@
+from tokenize import Number
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+
+    if not target_time:
+        return None
+
+    for element in permanence_period:
+        if type(element[0]) != int:
+            return None
+        if type(element[1]) != int:
+            return None
+
+    counter = 0
+
+    for element in permanence_period:
+        if element[0] == target_time:
+            counter += 1
+        if element[1] == target_time:
+            counter += 1
+
+    return counter
