eb56045868f55f70330082bb34ab4f1101ae8be0
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-27 22:02:34 -0400

    move function from challenge_palindromes_recursive to challenge_palindromes_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,21 @@
+def validate_word(word):
+    if not word or len(word) == 0:
+        return False
+
+
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if validate_word(word):
+        counter = 0
+        new_word = ""
+        word_len = len(word)
+
+        while counter < word_len:
+            new_word += word[word_len - counter]
+            counter += 1
+
+        if word == new_word:
+            return True
+        else:
+            return False
+    else:
+        return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,12 +1,2 @@
-def validate_word(word):
-    if not word or len(word) == 0:
-        return False
-
-    return True
-
-
 def is_palindrome_recursive(word, low_index, high_index):
-    if validate_word(word):
-        return print("foi")
-
-    return False
+    """aqui"""
