5b1ac60a071aab3df3220884d5a2516cb60ceff7
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-27 22:24:21 -0400

    add: palindrome test with list test

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,21 +1,16 @@
 def validate_word(word):
-    if not word or len(word) == 0:
+    if not word or word == "":
         return False
+
+    return True
 
 
 def is_palindrome_iterative(word):
-    if validate_word(word):
-        counter = 0
-        new_word = ""
-        word_len = len(word)
 
-        while counter < word_len:
-            new_word += word[word_len - counter]
-            counter += 1
+    # fonte:
+    # https://www.delftstack.com/pt/howto/python/python-palindrome/:
 
-        if word == new_word:
-            return True
-        else:
-            return False
+    if word == word[::-1]:
+        return True
     else:
         return False
