97216d14381f6f582316e7893a19d81907ca03e4
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-27 21:49:58 -0400

    add: validations

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,12 @@
+def validate_word(word):
+    if not word or len(word) == 0:
+        return False
+
+    return True
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if validate_word(word):
+        return print("foi")
+
+    return False
