e989a74312fa9d7b48f1fba4f1d3fc122efe58c8
Gustavo Sousa <gustavohl98@gmail.com>
2022-01-28 15:42:51 -0300

    Req 4 - find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
+    nums.sort()
+    nums_length = len(nums)
+    for index in range(nums_length - 1):
+        if isinstance(nums[index], int) and nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
     return False
