9790f92f7790eaf813b754a360e0b8a696d6c60b
Gustavo Sousa <gustavohl98@gmail.com>
2022-01-28 16:03:53 -0300

    Req 1 - refactoring to speed up algorithm

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,17 +1,10 @@
-def is_student_time_valid(student_hours):
-    if not isinstance(student_hours[0], int):
-        return False
-    if not isinstance(student_hours[1], int):
-        return False
-    return True
-
 
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
     counter = 0
     for student in permanence_period:
-        if not is_student_time_valid(student):
+        if not isinstance(student[0], int) or not isinstance(student[1], int):
             return None
         if student[0] <= target_time <= student[1]:
             counter += 1
