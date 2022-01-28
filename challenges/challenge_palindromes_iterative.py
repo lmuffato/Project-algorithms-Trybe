1c34b5fad05f2a4682f204e9013b73e2d1e93be3
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-27 22:28:11 -0400

    feat: Req 5 - Palíndromos (Iteratividade)

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -6,11 +6,14 @@ def validate_word(word):
 
 
 def is_palindrome_iterative(word):
+    if validate_word(word):
 
-    # fonte:
-    # https://www.delftstack.com/pt/howto/python/python-palindrome/:
+        # fonte:
+        # https://www.delftstack.com/pt/howto/python/python-palindrome/:
 
-    if word == word[::-1]:
-        return True
+        if word == word[::-1]:
+            return True
+        else:
+            return False
     else:
         return False
