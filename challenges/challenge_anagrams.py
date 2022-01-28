75e875436b345baeb6da8ec2b1846651462acee3
Marília Cegalla Aldrighi <76533793+maricegalla@users.noreply.github.com>
2022-01-28 20:43:07 -0300

    Removed comment

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -20,5 +20,3 @@ def is_anagram(first_string, second_string):
     if selection_sort(first_string) != selection_sort(second_string):
         return False
 
-#Fonte:
-#https://app.betrybe.com/course/computer-science/algoritmos/algoritmos-de-ordenacao-e-busca/29521083-44ea-488d-a74d-216b1ac79b04/conteudos/60672880-f607-40d3-92fc-e551b740a91f/algoritmos-de-ordenacao/fd503999-673b-443d-afb1-ffcc5d1718f4?use_case=side_bar
