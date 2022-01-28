00a20bf8461f32b093e0142712a1c793dbd44616
Gustavo Sousa <gustavohl98@gmail.com>
2022-01-28 15:58:56 -0300

    Req 1 - function study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,19 @@
+def is_student_time_valid(student_hours):
+    if not isinstance(student_hours[0], int):
+        return False
+    if not isinstance(student_hours[1], int):
+        return False
+    return True
+
+
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    counter = 0
+    for student in permanence_period:
+        if not is_student_time_valid(student):
+            return None
+        if student[0] <= target_time <= student[1]:
+            counter += 1
+
+    return counter
