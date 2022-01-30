1c02a485c9c0592101398b5645e0602b82528a73
Bidfca <bruninhakjurugat11@gmail.com>
2022-01-30 04:46:48 -0300

    5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+    i = 0
+    j = len(word) - 1
+    while i < j:
+        if word[i] != word[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,4 +8,4 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
     if low_index == math.floor(len(word) / 2):
         return True
-    return is_palindrome_recursive(word, low_index+1, high_index-1)
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
