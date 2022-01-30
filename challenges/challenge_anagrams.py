98c5c291956060d0db77bdbe2dafc4195a97e187
Bidfca <bruninhakjurugat11@gmail.com>
2022-01-30 04:31:07 -0300

    3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,34 @@
+def partition(word, low, high):
+    center = (high - low) >> 1
+    pivot = word[low + center]
+    i = low
+    j = high
+    while True:
+        while word[i] < pivot:
+            i = i + 1
+        while word[j] > pivot:
+            j = j - 1
+        if i >= j:
+            break
+        word[i], word[j] = word[j], word[i]
+        i = i + 1
+        j = j - 1
+    word[i], word[high] = word[high], word[i]
+    return i    
+
+
+def quick_sort(word, low, high):
+    if low < high:
+        pivot = partition(word, low, high)
+        quick_sort(word, low, pivot-1)
+        quick_sort(word, pivot, high)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_string = [*first_string]
+    second_string = [*second_string]
+    quick_sort(first_string, 0, len(first_string) - 1)
+    quick_sort(second_string, 0, len(second_string) - 1)
+    return first_string == second_string
