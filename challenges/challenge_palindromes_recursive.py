5322cb8053f455235e1798b3e8fa399fc31e58f9
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-03 22:30:04 -0300

    feat: req 2 test (será)?

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,6 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word[high_index] != word[low_index] or word is None:
+        return False
+    if low_index >= high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
