f5453278e18eda197f813b83f4d14552b8f2a560
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-04 09:39:04 -0400

    fear: Req 3 - Anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,17 @@
+# Solução encontrada no endereço
+# https://stackoverflow.com/questions/48217471/is-it-possible-to-check-for-anagram-without-using-sorted-or-dictionary-that-pe
+
+
+def build_counts(string):
+    char = {}
+    for c in string:
+        char[c] = char.setdefault(c, 0) + 1
+
+    return char
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = build_counts(first_string)
+    second = build_counts(second_string)
+
+    return first == second
