75b849f3d2494b86324399d975596d22841ad505
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-03 21:36:55 -0300

    feat: req 1 test

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+
+    n = 0
+    for i in permanence_period:
+        if i[0] is None or i[1] is None:
+            return None
+        elif i[0] <= target_time <= i[1]:
+            n += 1
+
+    return n
