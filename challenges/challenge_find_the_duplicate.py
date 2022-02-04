e9bcdcb52608921b01315d44d9b14026fc7c3731
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-03 20:50:12 -0400

    refactor validations on find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,17 +1,22 @@
-def find_duplicate(nums):
-    if len(nums) == 0:
-        return False
-
-    duplicated = None
+def search_duplicated(nums):
+    duplicated = False
     already_passed = set()
 
     for num in nums:
+        if type(num) == str or num < 0:
+            return False
         if num in already_passed:
             duplicated = num
         else:
             already_passed.add(num)
 
-    if not duplicated:
+    return duplicated
+
+
+def find_duplicate(nums):
+    if len(nums) == 0:
         return False
+
+    duplicated = search_duplicated(nums)
 
     return duplicated
