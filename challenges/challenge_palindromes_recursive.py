f36962d720d00eb9cbbf157802470752372f133b
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-02-04 10:20:00 -0400

    add: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """aqui"""
+    new_word = word[high_index]
+
+    if high_index < low_index:
+        return new_word == word[::-1]
+
+    return is_palindrome_recursive(word, low_index, high_index - 1)
