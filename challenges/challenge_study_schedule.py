ab1643b7a536af174632cea31aca468064aea27c
lucas-paz <lucas.hpaz@outlook.com>
2022-02-05 20:06:08 -0300

    Req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    if not target_time:
+        return None
+    online_students = 0
+    for in_time, out_time in permanence_period:
+        if not in_time or not out_time:
+            return None
+        if in_time <= target_time <= out_time:
+            online_students += 1
+    return online_students
