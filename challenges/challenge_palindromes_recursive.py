f9151b8b0ca8bcae1b68651a1c872f4d0c566cfe
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-05 12:57:06 -0300

    desafio 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    if low_index == len(word) // 2:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
