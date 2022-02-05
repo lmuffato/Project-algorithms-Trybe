76cb6c7fc771e3284998a79905b5fc749d2d5417
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-05 10:47:28 -0300

    desafio 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    pass
+    students = 0
+    try:
+        for start_time, exit_time in permanence_period:
+            if start_time <= target_time <= exit_time:
+                students += 1
+        return students
+    except TypeError:
+        return None
