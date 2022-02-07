b890c88ccd9037d40e1b47c1bbd5fc64440a62f5
Lucas Godoi <lucazgodoy1996@gmail.com>
2022-02-07 16:30:04 -0300

    Requirement 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    if not word:
+            return False
+    if len(word) == 1 or low_index > high_index:
+        return True
+    return word[low_index] == word[high_index] and is_palindrome_recursive(
+        word, low_index + 1, high_index - 1
+    )   
