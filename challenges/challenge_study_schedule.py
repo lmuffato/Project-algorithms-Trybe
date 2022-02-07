c54cf4d444ae11fc740bcc88059452e17fa9c68d
Lucas Godoi <lucazgodoy1996@gmail.com>
2022-02-07 16:21:04 -0300

    Requeriment 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,15 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    count = 0
+
+    if not target_time :
+        return None
+    
+    for entry, exit in permanence_period:
+        if not entry or not exit:
+            return None
+        if entry <= target_time <= exit  :
+            count += 1
+
+    return count
+
