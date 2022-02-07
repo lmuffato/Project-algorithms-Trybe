599249cb364e62fcbab8484458763a627def9d48
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-06 21:59:59 -0300

    desafio 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    for index in range(len(nums) - 1):
+        if isinstance(nums[index], int) and nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
+
