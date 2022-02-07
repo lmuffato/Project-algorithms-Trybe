a71456cda72a64c353c363b7a28db14ce62fb2de
Lucas Godoi <lucazgodoy1996@gmail.com>
2022-02-07 17:40:41 -0300

    Requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
     """ Faça o código aqui. """
+    if not word:
+        return False
+    palindromo = word[::-1]
+    result = "".join(palindromo)
+    if(result.lower() == word.lower()):
+        return True
+    return False
