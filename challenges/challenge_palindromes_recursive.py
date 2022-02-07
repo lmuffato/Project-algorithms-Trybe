042d028738fe0a3ff626e99ff803ba65270c9f9b
Lucas Godoi <lucazgodoy1996@gmail.com>
2022-02-07 17:07:45 -0300

    Requirement 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,9 +1,11 @@
+# Referência do código Lucas Paz
+
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
     if not word:
-            return False
+        return False
     if len(word) == 1 or low_index > high_index:
         return True
     return word[low_index] == word[high_index] and is_palindrome_recursive(
         word, low_index + 1, high_index - 1
-    )   
+    )
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,14 +2,12 @@ def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
     count = 0
 
-    if not target_time :
+    if not target_time:
         return None
-    
     for entry, exit in permanence_period:
         if not entry or not exit:
             return None
-        if entry <= target_time <= exit  :
+        if entry <= target_time <= exit:
             count += 1
 
     return count
-
