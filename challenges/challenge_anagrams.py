7d2f778479cf2457f0db7ad258bec7488929dc9e
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-02-07 12:00:10 -0300

    desafio 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    pass
+
+    if not first_string or not second_string:
+        return False
+
+    for letter in first_string:
+        # print('letter', letter)
+        counter_first = first_string.count(letter)
+        counter_second = second_string.count(letter)
+        # print('counter_first', counter_first)
+        # print('counter_second', counter_second)
+
+        if counter_first != counter_second:
+            return False
+    
+    return True
