7bd45f8a0b741255aa5e257f7cbe3a179338eda8
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-02-07 22:10:38 -0300

    Req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if len(word) // 2 == low_index:
+        return True
+    return is_palindrome_recursive(
+        word, low_index + 1, high_index - 1)
