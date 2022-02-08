00238bc18cc1a65fc58bb728a0f629470ff87c56
lucas-paz <lucas.hpaz@outlook.com>
2022-02-08 19:19:07 -0300

    fix infinite loop

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,13 +1,9 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-
     max_iterations = (len(word) // 2)
     has_difference = False
-
-    while not has_difference:
-        for i in range(0, max_iterations):
-            if word[i] != word[len(word) - (i + 1)]:
-                has_difference = True
-
-    return has_difference
+    for i in range(0, max_iterations):
+        if word[i] != word[len(word) - (i + 1)]:
+            has_difference = True
+    return not has_difference
