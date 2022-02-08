9d9ad4c31c156a89259f11f906724d99245169c6
lucas-paz <lucas.hpaz@outlook.com>
2022-02-07 21:27:46 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,31 @@
+# REF course;
+def do_sort(array):
+    has_swapped = True
+    num_of_iterations = 0
+
+    while has_swapped:
+        has_swapped = False
+
+        for i in range(len(array) - num_of_iterations - 1):
+            if array[i] > array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                has_swapped = True
+        num_of_iterations += 1
+
+    return array
+
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    if len(nums) == 0 or len(nums) == 1:
+        return False
+    result = False
+    ordered_nums = do_sort(nums)
+    for i in range(0, len(ordered_nums) - 1):
+        if type(ordered_nums[i]) != int or ordered_nums[i] < 0:
+            return False
+        if ordered_nums[i] == ordered_nums[i + 1]:
+            result = ordered_nums[i]
+            return result
+    return result
+
+    
