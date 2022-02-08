e6be289e7e14f66a4877a5834618d43a1a43da0e
lucas-paz <lucas.hpaz@outlook.com>
2022-02-08 19:09:41 -0300

    Req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,13 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if not word:
+        return False
+
+    max_iterations = (len(word) // 2)
+    has_difference = False
+
+    while not has_difference:
+        for i in range(0, max_iterations):
+            if word[i] != word[len(word) - (i + 1)]:
+                has_difference = True
+
+    return has_difference
