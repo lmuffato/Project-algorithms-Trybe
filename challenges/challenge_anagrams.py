e405f94b6069ed79694d6c607473b470ec6c7f88
phlopes <phlopes93@gmail.com>
2022-02-09 14:50:03 -0300

    req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def order_string(string):
+    ordered_string = ""
+    while len(ordered_string) < len(string):
+        lower = min(string)
+        ordered_string += lower
+        string = string.replace(lower, "", 1)
+    return ordered_string
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+    return order_string(first_string) == order_string(second_string)
+
+# Ref: https://github.com/tryber/sd-010-a-project-algorithms/pull/83/files
