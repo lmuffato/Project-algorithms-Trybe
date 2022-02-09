433bddb8c2dacf6479708b4162af8b5304f9f52a
phlopes <phlopes93@gmail.com>
2022-02-09 14:29:01 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    platform_access = 0
+
+    for entry, exit in permanence_period:
+        if not target_time:
+            return None
+        if not entry or not exit:
+            return None
+        if entry <= target_time <= exit:
+            platform_access += 1
+    return platform_access
