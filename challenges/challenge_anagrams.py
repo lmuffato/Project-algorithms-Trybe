4779cb75ebc2a8431342a409b4e1d8efb1c161a2
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-02-09 19:28:29 -0300

    Req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if not first_string or not second_string:
+        return False
+
+    for letter in first_string:
+        count_first = first_string.count(letter)
+        count_second = second_string.count(letter)
+        if count_first != count_second:
+            return False
+
+    return True
