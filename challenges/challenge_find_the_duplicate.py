c1dd33e7c1b968398b1a5ca27a455fc297aa3b88
phlopes <phlopes93@gmail.com>
2022-02-09 15:01:57 -0300

    req 04

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    for number in nums:
+        if type(number) != int or number < 0:
+            return False
+        if nums.count(number) > 1:
+            return number
+    return False
+
+# Ref:https://www.w3schools.com/python/ref_list_count.asp
+
