e1674331b97776fb2dc3ae5164b8242cc2b1105d
Gabriel Ferreira <glucas709@gmail.com>
2022-02-09 11:55:28 -0300

    requiriment 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    verified = []
+    repeated = None
+
+    for num in nums:
+        if type(num) != int or num < 1:
+            return False
+        
+        if num in verified:
+            repeated = num
+        verified.append(num)
+
+    return repeated if repeated else False
