7cb17390118971954e1e6fe3e0f3c25a96469137
Renan Costa <renancosta21@gmail.com>
2022-02-10 17:40:25 -0300

    req3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,40 @@
+# usado o código do course (bloco 35.3; merge_sort)
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+
+    return merge(left, right, array)
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    first_word = "".join(merge_sort(list(first_string)))
+    second_word = "".join(merge_sort(list(second_string)))
+
+    if first_word != second_word:
+        return False
+    return True
