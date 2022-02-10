14b956b1af0e5581cd9aa63345700ca8508b2299
Renan Costa <renancosta21@gmail.com>
2022-02-10 17:29:06 -0300

    req2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """ Faça o código aqui. """
+    # https://www.delftstack.com/pt/howto/python/python-palindrome/#:~:text=Primeiro%20calculamos%20o%20valor%20reverso,se%20n%C3%A3o%2C%20imprimimos%20Not%20Palindrome%20.
+    # https://www.geeksforgeeks.org/recursive-function-check-string-palindrome/
+    if not word:
+        return False
+    if low_index == high_index:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index < high_index + 1:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return True
