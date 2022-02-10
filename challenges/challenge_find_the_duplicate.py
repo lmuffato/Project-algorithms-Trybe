3069fddc30639975e63cce3249172ded67654698
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-02-09 22:32:19 -0300

    Req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,10 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    nums.sort()
+    nums_len = len(nums)
+    for index in range(nums_len - 1):
+        if isinstance(nums[index], int) and nums[index] < 0:
+            return False
+        if nums[index] == nums[index + 1]:
+            return nums[index]
+
+    return False
