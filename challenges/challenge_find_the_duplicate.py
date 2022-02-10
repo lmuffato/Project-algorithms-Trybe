2082c21a8d0805b3533bc30a954a452d03e7a9be
Renan Costa <renancosta21@gmail.com>
2022-02-10 18:10:37 -0300

    req4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
+# consultei o PR
+# https://github.com/tryber/sd-010-a-project-algorithms/pull/5/files
+
+
 def find_duplicate(nums):
     """ Faça o código aqui. """
+    sorted_nums = sorted(nums)
+    for index, num in enumerate(sorted_nums[:-1]):
+        if sorted_nums[index] == sorted_nums[index + 1] and num >= 0:
+            return num
+    return False
