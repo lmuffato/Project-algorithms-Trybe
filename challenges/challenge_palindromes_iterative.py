ca7bc726d88dd828869db4e296a43b870bc8d91d
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-02-10 00:57:17 -0300

    Req 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,9 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    word_len = len(word)
+
+    if word == "":
+        return False
+    for index in range(word_len // 2):
+        if word[index] != word[word_len - index - 1]:
+            return False
+    return True
