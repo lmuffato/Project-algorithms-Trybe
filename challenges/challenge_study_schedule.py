14fdd9d158a9d2cedb53efc2ee37a18cbf466cc2
Renan Costa <renancosta21@gmail.com>
2022-02-10 16:53:29 -0300

    req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
-    # linha para commit
+    if target_time is None:
+        return None
+    else:
+        logged_students = 0
+        for session in permanence_period:
+            # all() Retorna `True` se todos os elementos do iterável forem
+            # verdadeiros (ou se o iterável for vazio). Ou seja, a função
+            # recebe como parâmetro um objeto iterável (uma lista ou uma
+            # tupla, por exemplo) e verifica se todos os elementos desse
+            # iterável possuem o valor True
+            if not all(session):
+                return None
+            if session[0] <= target_time <= session[1]:
+                logged_students += 1
+    return logged_students
