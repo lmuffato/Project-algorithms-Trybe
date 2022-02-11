a1e2c10c48431bd034fdcc9b041b349acb304364
Breno <blf@ic.ufal.br>
2022-02-11 20:19:10 -0300

    feat: challenge 1 - study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if target_time is None:
+        return None
+    count = 0
+    for studant_period in permanence_period:
+        try:
+            if studant_period[0] <= target_time <= studant_period[1]:
+                count += 1
+        except TypeError:
+            return None
+    return count
