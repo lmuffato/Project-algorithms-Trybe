485825f300b651c34725b78411b8c127d174171d
EdmilsonBernardo <ebedmilson48@gmail.com>
2022-02-11 01:13:03 -0300

    Requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    students_on = 0
+
+    if not target_time:
+        return None
+
+    for in_time, out_time in permanence_period:
+        if not in_time or not out_time:
+            return None
+        if in_time <= target_time <= out_time:
+            students_on += 1
+
+    return students_on
