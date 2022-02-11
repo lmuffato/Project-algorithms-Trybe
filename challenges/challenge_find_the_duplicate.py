be2b03cf65f2163c021d61abb5e28a3488ebb350
EdmilsonBernardo <ebedmilson48@gmail.com>
2022-02-11 01:34:54 -0300

    Requirement 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,13 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    repeated_number = 0
+    array = []
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        if num in array:
+            repeated_number = num
+        else:
+            array.append(num)
+    if repeated_number == 0:
+        return False
+    return repeated_number
