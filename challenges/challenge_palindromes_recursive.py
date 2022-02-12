59522ccac9c133f00d3d82d374865727561317ca
lmuffato <lmiremuffato@gmail.com>
2022-02-12 20:01:07 -0300

    requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,18 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if not word or word == '':  # Se não conter o parâmerto word
+        return False
+    # Se as letras forem direfentes    
+    if word[low_index] != word[high_index]:
+        return False
+    # Quando chegar no meio da palavra
+    if low_index >= high_index:
+        return True
+    # Invoca a própria função recursivamente    
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+
+# Teste manual
+# print(is_palindrome_recursive('REVIVER', 0, 6))
+
+# Teste automatizado
+# python3 -m pytest tests/test_palindromes_recursive.py
--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
     usuarios_online = 0
-    # para cada tempo de entrada e saída em permanence_period
+    # para cada tempo de entrada e tempo de saída em permanence_period
     for tempo_inicio, tempo_saida in permanence_period:
         # Se não tiver tempo de entrada ou saída
         if not tempo_inicio or not tempo_saida:
