85d181d959038846d1e07d5e96be4f40ee08397d
ScriptCamilo <script.camilo@gmail.com>
2022-02-12 21:38:45 -0300

    Fourth requirement done

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,20 @@
+from challenges.challenge_anagrams import merge_sort
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    start = 0
+    end = len(nums) - 1
+
+    merge_sort(nums, start, end)
+
+    while len(nums) > 1:
+        last_number = nums.pop()
+
+        if type(last_number) is str:
+            break
+
+        if last_number < 0:
+            break
+
+        if (last_number == nums[-1]):
+            return last_number
+    return False
