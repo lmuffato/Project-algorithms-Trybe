b8776d1cee93ee723d77ca2b64a990c075b25566
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-12 21:28:37 -0300

    feat: req 3 test

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,10 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    word1 = list(first_string)
+    word2 = list(second_string)
+    anagram = ""
+    for gokuSsj in range(len(word1)):
+        if word2[gokuSsj] in word1:
+            anagram = "true"
+    if anagram != "true":
+        return False
+    return True
