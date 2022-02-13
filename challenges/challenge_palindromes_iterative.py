6032bd057b5ef8b98ad22c6d79d05dbc832a0de7
lmuffato <lmiremuffato@gmail.com>
2022-02-12 22:48:02 -0300

    requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,17 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    # string[::-1] -> retorna o inverso da entrada (stringa ou array)
+    inverted_word = word[::-1]
+    if not word or word == '':
+        return False
+    if word == inverted_word:
+        return True
+    if word != inverted_word:
+        return False
+
+
+# Teste manual
+# print(is_palindrome_iterative('amor'))
+
+
+# Teste automatizado
+# python3 -m pytest tests/test_palindromes_iterative.py
