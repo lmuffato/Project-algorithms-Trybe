56f8fbeaea22ecf53a644fd8fe84ab4011d5ff40
ScriptCamilo <script.camilo@gmail.com>
2022-02-12 21:47:41 -0300

    Fifth requirement done

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,12 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == '':
+        return False
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index < high_index:
+        if (word[low_index] != word[high_index]):
+            return False
+        low_index += 1
+        high_index -= 1
+    return True
