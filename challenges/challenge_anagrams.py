14b302f39706b80a2d2ba5cdd20670316a25dbed
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-12 21:35:40 -0300

    refac: conditional added req 33

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,6 @@
 def is_anagram(first_string, second_string):
+    if not first_string or not second_string:
+        return False
     word1 = list(first_string)
     word2 = list(second_string)
     anagram = ""
