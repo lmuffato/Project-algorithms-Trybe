1a6b9522cabaa2df9917db5f372f8cc30740b186
lmuffato <lmiremuffato@gmail.com>
2022-02-12 21:59:40 -0300

    requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,34 @@
+def sort_string(string):
+    text = ''
+    while len(string) > 0:
+        # monta a string com a menor letra da palavra
+        text += min(string)
+        # remove a menor letra da palavra original (apenas uma vez)
+        string = string.replace(min(string), '', 1)
+    return text
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    # no caso dos parametros não existirem
+    if not first_string or not first_string:
+        return False
+
+    if len(first_string) != len(second_string):
+        return False
+        
+    string_sorted_01 = sort_string(first_string)
+    string_sorted_02 = sort_string(second_string)
+    
+    index = 0
+
+    while index < len(string_sorted_01):
+        if string_sorted_01[index] != string_sorted_02[index]:
+            return False
+        index += 1
+    return True
+
+
+# Teste manual
+# print(is_anagram('pedrra', 'perrda'))
+
+# Teste automatizado
+# python3 -m pytest tests/test_anagrams.py
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,13 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if not word or word == '':  # Se não conter o parâmerto word
         return False
-    # Se as letras forem direfentes    
+    # Se as letras forem direfentes
     if word[low_index] != word[high_index]:
         return False
     # Quando chegar no meio da palavra
     if low_index >= high_index:
         return True
-    # Invoca a própria função recursivamente    
+    # Invoca a própria função recursivamente
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
 
 
