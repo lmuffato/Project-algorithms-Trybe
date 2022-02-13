d2726ab09f974e9e2dfa01a826190d5ec338755a
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-02-12 21:49:02 -0300

    feat: req 4 test

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,7 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    for gokuSSj in range(len(nums)):
+        for vegetaSsj in range(len(nums)):
+            if gokuSSj != vegetaSsj and nums[gokuSSj] == nums[vegetaSsj]:
+                if nums[gokuSSj] > 0:
+                    return nums[gokuSSj]
+    return False
